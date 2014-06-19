291b774547b10230112c69cbb166d9ce033918d9
Pål Trefall <admin@ptrefall.com>
2014-06-19 09:00:45 +0200

    Added a suggestion onto Entity for convenience. One can now gain direct access to Transform component from Entity if one so wishes. Not sure if we "really" want this...

--- include/GM/Framework/Entity.h
+++ include/GM/Framework/Entity.h
@@ -1,22 +1,30 @@
 #pragma once
 
-#include <Totem/ComponentContainer.h>
-#include <Totem/PropertyContainer.h>
-
+#include <Totem/Totem.h>
 #include <string>
 
 namespace GM {
 namespace Framework {
 
+class Transform; typedef std::shared_ptr<Transform> TransformPtr;
+
 class Entity : public Totem::PropertyContainer<>, public Totem::ComponentContainer<> {
 public:
 	Entity(const std::string &name);
-	virtual ~Entity() {};
+	virtual ~Entity();
 
 	std::string get_name() const { return name; };
 
+	TransformPtr get_transform(std::function<void(const TransformPtr &)> func = nullptr) const;
+	bool has_transform() const { return transform != nullptr; }
+
+private:
+	void on_component_added(std::shared_ptr<Totem::IComponent<>> component);
+
 private:
 	std::string name;
+
+	TransformPtr transform;
 };
 
 } // namespace Framework
--- src/Framework/Entity.cpp
+++ src/Framework/Entity.cpp
@@ -1,7 +1,23 @@
 #include "GM/Framework/Entity.h"
-
+#include "GM/Framework/Components/Transform.h"
 using namespace GM::Framework;
 
 Entity::Entity(const std::string &name) : name(name) {
+	componentAdded().connect(clan::Callback<void(std::shared_ptr<Totem::IComponent<>>)>(this, &Entity::on_component_added));
+}
 
+Entity::~Entity() {
+
+}
+
+void Entity::on_component_added(std::shared_ptr<Totem::IComponent<>> component) {
+	if (Totem::IComponent<>::isType<Transform>(component)) {
+		transform = std::static_pointer_cast<Transform>(component);
+	}
+}
+
+TransformPtr Entity::get_transform(std::function<void(const TransformPtr &)> func) const {
+	if (func && has_transform()) func(transform);
+
+	return transform;
 }
