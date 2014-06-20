1f957a3e9ba6f1547a0a3979a18e40d79fc3e737
Endre Oma <endre.88.oma@gmail.com>
2014-06-20 21:02:37 +0200

    Add get_{world,object}_matric(), is_culled(), is_visible() to IRenderable

--- include/GM/Framework/Components/IRenderable.h
+++ include/GM/Framework/Components/IRenderable.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <glm/glm.hpp>
+
 #include <memory>
 
 namespace GM {
@@ -11,6 +13,14 @@ class IRenderable {
 public:
 	virtual ~IRenderable() = 0;
 
+	virtual glm::mat4 get_world_matrix() = 0;
+	virtual glm::mat4 get_object_matrix() = 0;
+
+	virtual bool is_culled() = 0;
+
+	// should test if it heas been culled or just not visible
+	// eg. return (culled || !visible)
+	virtual bool is_invisible() = 0;
 };
 
 } // namespace Framework
