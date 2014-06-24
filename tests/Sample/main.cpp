a2162bb0b87c502d7a69a21ef653de4f4eff4954
Endre Oma <endre.88.oma@gmail.com>
2014-06-24 17:34:33 +0200

    Make tests conform to the integrated Totem

--- tests/Sample/main.cpp
+++ tests/Sample/main.cpp
@@ -10,33 +10,17 @@
 using namespace GM;
 using namespace Framework;
 
-void on_component_added(std::shared_ptr<Totem::IComponent<>> component) {
+void on_component_added(std::shared_ptr<IComponent<>> component) {
 	std::string owner_name = "Unknown";
-	if (Totem::IComponent<>::isType<Transform>(component)) {
+	if (IComponent<>::is_type<Transform>(component)) {
 		auto transform = std::static_pointer_cast<Transform>(component);
 		owner_name = transform->get_owner()->get_name();
 	}
-	else if (Totem::IComponent<>::isType<Renderable>(component)) {
+	else if (IComponent<>::is_type<Renderable>(component)) {
 		auto renderable = std::static_pointer_cast<Renderable>(component);
 		owner_name = renderable->get_owner()->get_name();
 	}
-	std::cout << "Component " << component->getName() << " was added to " << owner_name << "!" << std::endl;
-}
-
-void on_component_removed(std::shared_ptr<Totem::IComponent<>> component) {
-	std::string owner_name = "Unknown";
-	if (Totem::IComponent<>::isType<Transform>(component)) {
-		auto transform = std::static_pointer_cast<Transform>(component);
-		owner_name = transform->get_owner()->get_name();
-	}
-	else if (Totem::IComponent<>::isType<Renderable>(component)) {
-		auto renderable = std::static_pointer_cast<Renderable>(component);
-		owner_name = renderable->get_owner()->get_name();
-	}
-	if (owner_name.empty())
-		std::cout << "Component " << component->getName() << " was removed from its entity!" << std::endl;
-	else
-		std::cout << "Component " << component->getName() << " was removed from " << owner_name << "!" << std::endl;
+	std::cout << "Component " << component->get_name() << " was added to " << owner_name << "!" << std::endl;
 }
 
 void on_transform_child_added(const Transform * const parent, const Transform * const child) {
@@ -51,21 +35,16 @@ bool mainTest() {
 	auto scene_manager = std::make_shared<SceneManager>();
 
 	auto root_entity = entity_manager->create_entity("Root");
-	root_entity->componentAdded().connect(clan::Callback<void(std::shared_ptr<Totem::IComponent<>>)>(&on_component_added));
-	root_entity->componentRemoved().connect(clan::Callback<void(std::shared_ptr<Totem::IComponent<>>)>(&on_component_removed));
-	auto root_transform = root_entity->addComponent<Transform>(std::make_shared<Transform>(root_entity, scene_manager, Transform::get_static_type()));
+	root_entity->component_added().connect(clan::Callback<void(std::shared_ptr<IComponent<>>)>(&on_component_added));
+	auto root_transform = root_entity->add_component<Transform>(std::make_shared<Transform>(root_entity, scene_manager, Transform::get_static_type()));
 	root_transform->get_child_added_signal().connect(CallbackParentChild(&on_transform_child_added));
 
 	auto child_entity = entity_manager->create_entity("Child");
-	child_entity->componentAdded().connect(clan::Callback<void(std::shared_ptr<Totem::IComponent<>>)>(&on_component_added));
-	child_entity->componentRemoved().connect(clan::Callback<void(std::shared_ptr<Totem::IComponent<>>)>(&on_component_removed));
-	auto child_transform = child_entity->addComponent<Transform>(std::make_shared<Transform>(child_entity, scene_manager, Transform::get_static_type()));
+	child_entity->component_added().connect(clan::Callback<void(std::shared_ptr<IComponent<>>)>(&on_component_added));
+	auto child_transform = child_entity->add_component<Transform>(std::make_shared<Transform>(child_entity, scene_manager, Transform::get_static_type()));
 	child_transform->get_child_added_signal().connect(CallbackParentChild(&on_transform_child_added));
 
 	root_transform->add_child(child_transform);
-
-	scene_manager.reset();
-	entity_manager.reset();
 
 	return true;
 }
