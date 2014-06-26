1bd1b57a5c56cb922507d437b2b87c1d68d12dca
Endre Oma <endre.88.oma@gmail.com>
2014-06-26 13:29:34 +0200

    Make renderable conform to the revised interface

--- include/GM/Framework/Components/Renderable.h
+++ include/GM/Framework/Components/Renderable.h
@@ -16,16 +16,32 @@ class RenderSystem; typedef std::shared_ptr<RenderSystem> RenderSystemPtr;
 
 class Renderable : public IRenderable, public Component<Renderable> {
 public:
-	Renderable(const EntityPtr &owner, const RenderSystemPtr &render_system, const std::string &name = std::string());
+	Renderable(const EntityPtr &owner, const RenderSystemPtr &render_system, unsigned int render_layers = 1, const std::string &name = std::string());
 	virtual ~Renderable();
 
 	std::string get_type() const override { return get_static_type(); }
 
+	virtual	glm::mat4 get_world_matrix() const override { return world_matrix_property; };
+	virtual	glm::mat4 get_object_matrix() const override { return object_matrix_property; };
+
+	virtual bool is_culled() const override { return culled_property; };
+	virtual bool is_invisible() const override { return !visible_property || is_culled(); };
+
+	virtual unsigned int get_render_layers() const override { return render_layers; };
+
+	virtual void render() override;
 public:
 	static std::string get_static_type() { return COMPONENT_RENDERABLE; }
 
 private:
 	RenderSystemPtr render_system;
+	const unsigned int render_layers;
+
+	Property<glm::mat4> world_matrix_property;
+	Property<glm::mat4> object_matrix_property;
+
+	Property<bool> culled_property;
+	Property<bool> visible_property;
 };
 
 } // namespace Framework
--- src/Framework/Components/Renderable.cpp
+++ src/Framework/Components/Renderable.cpp
@@ -1,16 +1,28 @@
 #include "GM/Framework/Components/Renderable.h"
-
+#include "GM/Framework/DefinitionsPropertyNames.h"
+#include "GM/Framework/Entity.h"
 #include "GM/Framework/Systems/RenderSystem.h"
 
 using namespace GM::Framework;
 
-Renderable::Renderable(const EntityPtr &owner, const RenderSystemPtr &render_system, const std::string &name)
+Renderable::Renderable(const EntityPtr &owner, const RenderSystemPtr &render_system, const unsigned int render_layers, const std::string &name)
 : Component<Renderable>(owner, name)
 , render_system(render_system)
+, render_layers(render_layers)
 {
 	render_system->add_renderable(this);
+
+	world_matrix_property = owner->add(PROPERTY_WORLD_MATRIX, glm::mat4());
+	object_matrix_property = owner->add(PROPERTY_OBJECT_MATRIX, glm::mat4());
+
+	culled_property = owner->add(PROPERTY_CULLED, false);
+	visible_property = owner->add(PROPERTY_VISIBLE, false);
 }
 
 Renderable::~Renderable() {
 	render_system->remove_renderable(this);
+}
+
+void Renderable::render() {
+
 }
