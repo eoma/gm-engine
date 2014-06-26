9d3fc948e991cf5d2126ba01a4b1fc674255e4e4
Endre Oma <endre.88.oma@gmail.com>
2014-06-26 13:14:17 +0200

    Add methods render(), get_render_layers() to IRenderable, make some methods const

--- include/GM/Framework/Components/IRenderable.h
+++ include/GM/Framework/Components/IRenderable.h
@@ -13,14 +13,22 @@ class IRenderable {
 public:
 	virtual ~IRenderable() {};
 
-	virtual glm::mat4 get_world_matrix() = 0;
-	virtual glm::mat4 get_object_matrix() = 0;
+	virtual glm::mat4 get_world_matrix() const = 0;
+	virtual glm::mat4 get_object_matrix() const = 0;
 
-	virtual bool is_culled() = 0;
+	virtual bool is_culled() const = 0;
 
 	// should test if it heas been culled or just not visible
 	// eg. return (culled || !visible)
-	virtual bool is_invisible() = 0;
+	virtual bool is_invisible() const = 0;
+
+	// Which render layers do you want to be in?
+	// The format is defined as a bitfield. If you want
+	// to be in layer 0, 1 and 2, you return 1<<0 + 1<<1 + 1<<2.
+	// Default should be 1.
+	virtual unsigned int get_render_layers() const = 0;
+
+	virtual void render() = 0;
 };
 
 } // namespace Framework
