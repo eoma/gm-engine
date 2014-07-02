74107ad136752d90a720f1447fe455a64d306940
Endre Oma <endre.88.oma@gmail.com>
2014-07-02 17:16:39 +0200

    Add function find_path_in_hierarchy in GM/Framework/Utilities/Tools.h

---
+++ include/GM/Framework/Utilities/Tools.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+
+namespace GM {
+	namespace Framework {
+		/**
+		 * Locates a path based on path hierarchgy. Will only search upward the hierarchy.
+		 *
+		 * @param start_path The starting point, eg. the path to your executable.
+		 * @param wanted_end_of_path what the resulting path must contain.
+		 * @param max_ancsetor_length how many steps up the directory hierarchy are allowed to search.
+		 * @return the found path
+		 * @throws std::runtime_error if path is not found.
+		 */
+		std::string find_path_in_hierarchy(const std::string &start_path, const std::string &wanted_end_of_path, const unsigned int max_ancestor_length = 100);
+	}
+}
---
+++ src/Framework/Utilities/Tools.cpp
@@ -0,0 +1,34 @@
+#include "GM/Framework/Utilities/Tools.h"
+
+#include <ClanLib/core.h>
+
+std::string GM::Framework::find_path_in_hierarchy(const std::string &start_path, const std::string &wanted_end_of_path, const unsigned int max_ancestor_length) {
+	bool found = false;
+	std::string root_path = start_path;
+	std::string path = clan::PathHelp::make_absolute(root_path, wanted_end_of_path);
+
+	// We use the directory scanner to determine if a folder can be scanned
+	// and thereby determine whether it exists or not.
+	clan::DirectoryScanner directory_scanner;
+
+	for (unsigned int i = 0; !found && i < max_ancestor_length; ++i) {
+		found = directory_scanner.scan(path);
+
+		if (!found) {
+			try {
+				root_path = clan::PathHelp::make_absolute(root_path, "../");
+			} catch (clan::Exception e) {
+				// We are unable to get firther down the hiearchy, break out of loop
+				break;
+			}
+
+			path = clan::PathHelp::make_absolute(root_path, wanted_end_of_path);
+		}
+	}
+
+	if (!found) {
+		throw std::runtime_error("Unable to find path");
+	}
+	
+	return path;
+}
