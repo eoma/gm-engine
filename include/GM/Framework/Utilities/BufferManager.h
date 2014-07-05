63b454a2d3107240fdc86355010d5ea190588c1c
Endre Oma <endre.88.oma@gmail.com>
2014-07-05 19:32:30 +0200

    Add implementation of a buffer manager

---
+++ include/GM/Framework/Utilities/BufferManager.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <vector>
+
+namespace GM {
+namespace Framework {
+
+class BufferManager {
+public:
+
+	struct BufferAllocation
+	{
+		unsigned int name;
+		unsigned int allocated_size;
+		unsigned int offset;
+	};
+
+	BufferManager(unsigned int default_pool_size = 32 * (1<<20));
+	~BufferManager();
+
+	BufferAllocation allocate(const unsigned int size, const bool force_unique = false);
+
+private:
+	struct PoolData
+	{
+		unsigned int name;
+		unsigned int total_size;
+		unsigned int allocated;
+		unsigned int content_type; // 
+		unsigned int use_type; // read,write or copy; static, dynamic or stream
+
+		unsigned int get_unused_size() const { return total_size - allocated; };
+
+		bool has_space_for(const unsigned int size) const { return size <= get_unused_size(); };
+
+		// Increases allocated by size and returns previous allocated as offset
+		unsigned int allocate(const unsigned int size)
+		{
+			unsigned int offset = allocated;
+
+			allocated += size;
+
+			return offset;
+		}
+
+		PoolData()
+		: name(0)
+		, total_size(0)
+		, allocated(0)
+		, content_type(0)
+		, use_type(0)
+		{
+		}
+	};
+
+// Remove when gl is introduced
+#define GL_ARRAY_BUFFER 0
+#define GL_STATIC_DRAW 0
+
+
+private:
+
+	PoolData create_pool(unsigned int size, unsigned int content_type = GL_ARRAY_BUFFER, unsigned int use_type = GL_STATIC_DRAW);
+
+private:
+	unsigned int default_pool_size;
+
+	std::vector<PoolData> pools;
+};
+
+} // namespace Framework
+} // namespace GM
---
+++ src/Framework/Utilities/BufferManager.cpp
@@ -0,0 +1,79 @@
+#include "GM/Framework/Utilities/BufferManager.h"
+
+#include <algorithm>
+
+namespace GM {
+namespace Framework {
+
+BufferManager::BufferManager(unsigned int default_pool_size)
+: default_pool_size(default_pool_size)
+{
+}
+
+BufferManager::~BufferManager()
+{
+	for (const PoolData &pool : pools) {
+		// TODO: Deallocate storage
+	}
+}
+
+BufferManager::BufferAllocation BufferManager::allocate(const unsigned int size, const bool force_unique)
+{
+	BufferAllocation allocation;
+	std::vector<PoolData>::iterator iter;
+
+	if (!force_unique) // we have the freedom to allocate where it seems fit
+	{
+		// Locate a a pool with enough vacant size
+		iter = std::find_if(pools.begin(), pools.end(), 
+			[size](const PoolData &pool) { return pool.has_space_for(size); }
+		);
+
+		if (iter == pools.end()) // Must allocate new pool
+		{
+			unsigned int pool_size = default_pool_size;
+			if (size > default_pool_size) pool_size = size;
+
+			PoolData pool = create_pool(pool_size);
+
+			pools.push_back(pool);
+			iter = --(pools.end());
+		}
+	}
+	else
+	{
+		PoolData pool = create_pool(size);
+
+		pools.push_back(pool);
+		iter = --(pools.end());
+	}
+
+	PoolData &pool = *iter;
+
+	allocation.name = pool.name;
+	allocation.allocated_size = size;
+	allocation.offset = pool.allocate(size);
+
+	return allocation;
+}
+
+BufferManager::PoolData BufferManager::create_pool(unsigned int size, unsigned int content_type, unsigned int use_type)
+{
+	PoolData pool;
+
+	// Generate buffer, Core::make_buffer?
+	pool.name = 1;
+
+	pool.total_size = size;
+	pool.content_type = content_type;
+	pool.use_type = use_type;
+
+	//
+	// TODO: Do something to allocate storage
+	//
+
+	return pool;
+}
+
+} // namespace Framework
+} // namespace GM
