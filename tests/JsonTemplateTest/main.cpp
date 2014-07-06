2699e8f6dfb949a153a1418bc0cffb82fdb0fcfc
Endre Oma <endre.88.oma@gmail.com>
2014-07-06 10:37:15 +0200

    JSON files should have extension .json

--- tests/JsonTemplateTest/main.cpp
+++ tests/JsonTemplateTest/main.cpp
@@ -81,7 +81,7 @@ bool mainTest() {
 	path = find_path_in_hierarchy(path, wanted_directory);
 	std::cout << "Resource path: " << path << std::endl;
 
-	template_manager->add_templates(path + "/test_entity_template1.js");
+	template_manager->add_templates(path + "/test_entity_template1.json");
 
 	auto entity1 = entity_manager->create_entity("One");
 	auto entity2 = entity_manager->create_entity("Two");
