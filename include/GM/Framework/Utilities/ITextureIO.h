512b50d7b959518edb2ec735284ab638dc7a1f36
Endre Oma <endre.88.oma@gmail.com>
2014-07-12 19:52:32 +0200

    Add interface ITextureIO for loading images as textures

---
+++ include/GM/Framework/Utilities/ITextureIO.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "RawImage.h"
+
+#include <string>
+#include <vector>
+
+namespace GM {
+namespace Framework {
+
+class ITextureIO {
+public:
+	virtual ~ITextureIO() = 0;
+
+	/**
+	 * Load image from file.
+	 * Should basically load a file and call ITextureIO::load(pointer to start, size))
+	 */
+	virtual RawImage load(const std::string &file_name) = 0;
+
+	/**
+	 * Load undecoded image from memory, assumes vector of unsigned chars.
+	 * Basically calls load(undecoded_image.data(), undecoded_data.size()).
+	 */
+	virtual RawImage load(const std::vector<unsigned char> &undecoded_image) = 0;
+
+	/**
+	 * Load undecoded image from memory, assumes array of unsigned char
+	 */
+	virtual RawImage load(const unsigned char * const undecoded_image, const unsigned int size) = 0;
+
+	/**
+	 * Save image to file. The file name must end with a valid 
+	 * image extensions. It is not guaranteed that the implementation
+	 * will support the given image type. Implmentations should
+	 * support bmp and png.
+	 *
+	 * @return true if image successfully saved
+	 */
+	virtual bool save(const std::string &file_name, const RawImage &image) = 0;
+};
+
+} // namespace Framework
+} // namespace GM
