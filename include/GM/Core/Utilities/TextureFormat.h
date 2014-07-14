f882a5d60da304f2e35b84ce429a04ab4a744f8e
Endre Oma <endre.88.oma@gmail.com>
2014-07-14 22:26:36 +0200

    Add texture format holder

---
+++ include/GM/Core/Utilities/TextureFormat.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include "TexParameter.h"
+
+#include <string>
+#include <vector>
+
+namespace GM {
+namespace Core {
+
+/**
+ * Texture format is just a container of parameters..
+ * You should add specializations that derive from this container to make eg. 2D textures or cube maps
+ */
+class TextureFormat
+{
+public:
+	virtual ~TextureFormat() {};
+	
+	unsigned int get_type() const { return type; };
+	bool get_make_mipmap() const { return make_mipmap; };
+
+	const std::vector<std::string> &get_image_files() const { return image_files; };
+
+	const std::vector<TexParameterInt> &get_int_parameters() const { return int_parameters; };
+	const std::vector<TexParameterFloat> &get_float_parameters() const { return float_parameters; };
+
+	static int compare(const TextureFormat &current, const TextureFormat &other);
+
+protected:
+	// TODO: A bit unclear a bout these, why should they be protected?
+	TextureFormat(const unsigned int type);
+
+	void set_make_mipmap(bool value) { make_mipmap = value; };
+
+	void add_image_file(const std::string &file_name) { image_files.push_back(file_name); };
+
+	void set_parameter(unsigned int param_name, float param) { float_parameters.push_back(TexParameterFloat(param_name, param)); };
+	void set_parameter(unsigned int param_name, int param) { int_parameters.push_back(TexParameterInt(param_name, param)); };
+
+protected:
+	const unsigned int type; // texture format type
+	bool make_mipmap; // should we create a mipmap for the(se) texture(s)?
+
+	// Can _not_ be sorted, order must be preserved
+	std::vector<std::string> image_files;
+
+	std::vector<TexParameterInt> int_parameters;
+	std::vector<TexParameterFloat> float_parameters;
+};
+
+bool operator< (const TextureFormat &current, const TextureFormat &other);
+
+// TODO: class or function?
+class Texture2D : public TextureFormat
+{
+public:
+	// FIXME: Set proper defines when GL is introdused
+	Texture2D(const std::string &file_name) : TextureFormat(/*GL_TEXTURE_2D*/ 1)
+	{
+		add_image_file(file_name);
+
+		// FIXME: Uncomment when GL is introduced
+		//set_parameter(GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		//set_parameter(GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		//set_parameter(GL_TEXTURE_WRAP_S, wrap_mode);
+		//set_parameter(GL_TEXTURE_WRAP_T, wrap_mode);
+
+		set_make_mipmap(true);
+	};
+
+	virtual ~Texture2D() {};
+};
+
+} // namespace Core
+} // namespace GM
---
+++ src/Core/Utilities/TextureFormat.cpp
@@ -0,0 +1,33 @@
+#include "GM/Core/Utilities/TextureFormat.h"
+
+namespace GM {
+namespace Core {
+
+TextureFormat::TextureFormat(const unsigned int type)
+: type(type)
+, make_mipmap(false)
+, image_files()
+, int_parameters()
+, float_parameters()
+{
+};
+
+int TextureFormat::compare(const TextureFormat &current, const TextureFormat &other)
+{
+	int result = current.type - other.type;
+
+	if (result == 0) result = current.make_mipmap - other.make_mipmap;
+	if (result == 0) result = (current.image_files < other.image_files) ? -1 : (other.image_files < current.image_files ? 1 : 0);
+	if (result == 0) result = (current.int_parameters < other.int_parameters) ? -1 : (other.int_parameters < current.int_parameters ? 1 : 0);
+	if (result == 0) result = (current.float_parameters < other.float_parameters) ? -1 : (other.float_parameters < current.float_parameters ? 1 : 0);
+
+	return result;
+};
+
+bool operator< (const TextureFormat &current, const TextureFormat &other)
+{
+	return TextureFormat::compare(current, other);
+}
+
+} // namespace Core
+} // namespace GM
