39252354eade59eb6dee2ce3e45b518111c31da3
Endre Oma <endre.88.oma@gmail.com>
2014-07-15 14:38:30 +0200

    Make RawImage store images as a shared pointer to a vector

--- include/GM/Framework/Utilities/RawImage.h
+++ include/GM/Framework/Utilities/RawImage.h
@@ -1,15 +1,16 @@
 #pragma once
 
+#include <memory>
 #include <vector>
 
 class RawImage {
 public:
-	RawImage() : data(), width(0), height(0), num_channels(0) {};
+	RawImage() : dataptr(std::make_shared<std::vector<unsigned char>>()), width(0), height(0), num_channels(0) {};
 	
 	RawImage(const std::vector<unsigned char> &decoded_data,
 		const unsigned int width, const unsigned int height, 
 		const unsigned int num_channels)
-	: data(decoded_data)
+	: dataptr(std::make_shared<std::vector<unsigned char>>(decoded_data))
 	, width(width)
 	, height(height)
 	, num_channels(num_channels)
@@ -19,21 +20,22 @@ public:
 	RawImage(const unsigned char * decoded_data,
 		const unsigned int width, const unsigned int height,
 		const unsigned int num_channels)
-	: data(decoded_data, decoded_data + (width*height*num_channels))
+	: dataptr(std::make_shared<std::vector<unsigned char>>(decoded_data, decoded_data + (width*height*num_channels)))
 	, width(width)
 	, height(height)
 	, num_channels(num_channels)
 	{
 	}
 
-	const std::vector<unsigned char> &get_data() const { return data; };
+	const std::vector<unsigned char> &get_data() const { return *dataptr; };
+	const std::shared_ptr<std::vector<unsigned char>> get_dataptr() const { return dataptr; };
 
 	unsigned int get_width() const { return width; };
 	unsigned int get_height() const { return height; };
 	unsigned int get_num_channels() const { return num_channels; };
 
 private:
-	std::vector<unsigned char> data;
+	std::shared_ptr<std::vector<unsigned char>> dataptr;
 	unsigned int width;
 	unsigned int height;
 	unsigned int num_channels;
