652e6914d0832a1985b40582b389a08bd40aa2c2
Endre Oma <endre.88.oma@gmail.com>
2014-07-15 13:11:13 +0200

    When comparing vectors of shared_ptrs use std::owner_less and std::lexicographical_compare

--- src/Core/Utilities/TextureFormat.cpp
+++ src/Core/Utilities/TextureFormat.cpp
@@ -1,5 +1,7 @@
 #include "GM/Core/Utilities/TextureFormat.h"
 
+#include <algorithm>
+
 namespace GM {
 namespace Core {
 
@@ -16,7 +18,29 @@ int TextureFormat::compare(const TextureFormat &current, const TextureFormat &ot
 
 	if (result == 0) result = current.make_mipmap - other.make_mipmap;
 	if (result == 0) result = (current.image_files < other.image_files) ? -1 : (other.image_files < current.image_files ? 1 : 0);
-	if (result == 0) result = (current.parameters < other.parameters) ? -1 : (other.parameters < current.parameters ? 1 : 0);
+	if (result == 0)
+	{
+		// test if current.parameters < other.parameters
+		bool less_than = std::lexicographical_compare(
+			current.parameters.begin(), current.parameters.end(),
+			other.parameters.begin(), other.parameters.end(),
+			std::owner_less<ITextureParameterPtr>());
+
+		if (less_than)
+		{
+			result = -1;
+		}
+		else
+		{
+			// test if other.parameters < other.parameters
+			less_than = std::lexicographical_compare(
+				other.parameters.begin(), other.parameters.end(),
+				current.parameters.begin(), current.parameters.end(),
+				std::owner_less<ITextureParameterPtr>());
+
+			if (less_than) result = 1;
+		}
+	}
 
 	return result;
 };
