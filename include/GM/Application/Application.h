e22c43a8588e672970bed04f3bddd491783437c5
Pål Trefall <admin@ptrefall.com>
2014-07-19 14:37:16 +0200

    Did a bit more implementation on Application.

--- include/GM/Application/Application.h
+++ include/GM/Application/Application.h
@@ -32,23 +32,39 @@ public:
 
 	enum Flags
 	{
-		GM_SCENE_SYSTEM = (1<<0),
-		GM_RENDER_SYSTEM = (1<<1),
-		GM_ENTITY_MANAGER = (1<<2),
-		GM_BUFFER_MANAGER = (1<<3),
-		GM_SHADER_MANAGER = (1<<4),
-		GM_TEXTURE_MANAGER = (1<<5),
-		GM_VAO_MANAGER = (1<<6),
+		GM_FRAMEWORK_SCENE_SYSTEM = (1 << 0),
+		GM_FRAMEWORK_RENDER_SYSTEM = (1 << 1),
+		GM_FRAMEWORK_ENTITY_MANAGER = (1 << 2),
+		GM_FRAMEWORK_BUFFER_MANAGER = (1 << 3),
+		GM_FRAMEWORK_SHADER_MANAGER = (1 << 4),
+		GM_FRAMEWORK_TEXTURE_MANAGER = (1 << 5),
+		GM_FRAMEWORK_VAO_MANAGER = (1 << 6),
 
-		GM_NO_DEFAULTS = 0,
-		GM_FRAMEWORK_DEFAULTS = ((1<<30) - 1)
+		GM_FRAMEWORK_NO_DEFAULTS = 0,
+		GM_FRAMEWORK_ALL_DEFAULTS = ((1<<30) - 1)
+	};
+
+	enum ErrorFlags
+	{
+		GM_ERROR_NULL_SYSTEM = (1 << 0),
+		GM_ERROR_EXCEPTION = (1 << 1),
+		GM_ERROR_GL_DEBUG_OUTPUT = (1 << 2),
+
+		GM_ERROR_NO_CHECKS = 0,
+		GM_ERROR_ALL_CHECKS = ((1<<30) - 1)
 	};
 
 public:
-	Application(const std::string &title, Application::Flags flags, unsigned int width = 800, unsigned int height = 640, bool fullscreen = false);
+	Application(const std::string &title, Application::Flags flags = Flags::GM_FRAMEWORK_ALL_DEFAULTS, Application::ErrorFlags error_flags = GM_ERROR_ALL_CHECKS, unsigned int width = 800, unsigned int height = 640, bool fullscreen = false);
 	virtual ~Application();
 
 	void run();
+
+	const std::string &get_title() const { return title; }
+	unsigned int get_width() const { return width; }
+	unsigned int get_height() const { return height; }
+	bool is_fullscreen() const { return fullscreen; }
+	bool is_running() const { return keep_running; }
 
 	clan::Signal<void()> &on_initialize() { return initialize_sign; }
 	clan::Signal<void(double)> &on_update() { return update_sign; }
@@ -56,30 +72,47 @@ public:
 	clan::Signal<void()> &on_render() { return render_sign; }
 	clan::Signal<void()> &on_clean_up() { return clean_up_sign; }
 
+	// FIXME: All setters should probably ensure that the this-> version is nullptr and not already set...?
+	// FIXME: Else throw an exception?
 
 	bool has_scene_system() const { return scene_system != nullptr; }
 	const Framework::SceneSystemPtr &get_scene_system() const { return scene_system; }
+	void set_scene_system(const Framework::SceneSystemPtr &scene_system) { this->scene_system = scene_system; }
 
 	bool has_render_system() const { return render_system != nullptr; }
 	const Framework::RenderSystemPtr &get_render_system() const { return render_system; }
+	void set_render_system(const Framework::RenderSystemPtr &render_system) { this->render_system = render_system; }
 
 	bool has_entity_manager() const { return entity_manager != nullptr; }
 	const Framework::EntityManagerPtr &get_entity_manager() const { return entity_manager; }
+	void set_entity_manager(const Framework::EntityManagerPtr &entity_manager) { this->entity_manager = entity_manager; }
 
 	bool has_buffer_manager() const { return buffer_manager != nullptr; }
 	const Framework::BufferManagerPtr &get_buffer_manager() const { return buffer_manager; }
+	void set_buffer_manager(const Framework::BufferManagerPtr &buffer_manager) { this->buffer_manager = buffer_manager; }
 
 	bool has_shader_manager() const { return shader_manager != nullptr; }
 	const Framework::ShaderManagerPtr &get_shader_manager() const { return shader_manager; }
+	void set_shader_manager(const Framework::ShaderManagerPtr &shader_manager) { this->shader_manager = shader_manager; }
 
 	bool has_texture_manager() const { return texture_manager != nullptr; }
 	const Framework::TextureManagerPtr &get_texture_manager() const { return texture_manager; }
+	void set_texture_manager(const Framework::TextureManagerPtr &texture_manager) { this->texture_manager = texture_manager; }
 
 	bool has_vao_manager() const { return vao_manager != nullptr; }
 	const Framework::VaoManagerPtr &get_vao_manager() const { return vao_manager; }
+	void set_vao_manager(const Framework::VaoManagerPtr &vao_manager) { this->vao_manager = vao_manager; }
 
 protected:
+	void initialize();
+	void update();
+	void prepare();
+	void render();
+	void clean_up();
+
 	GLFWwindow *window;
+
+	Application::ErrorFlags error_flags;
 
 	Framework::SceneSystemPtr scene_system;
 	Framework::RenderSystemPtr render_system;
--- src/Application/Application.cpp
+++ src/Application/Application.cpp
@@ -14,9 +14,10 @@
 namespace GM {
 namespace Application {
 
-Application::Application(const std::string &title, Application::Flags flags, unsigned int width, unsigned int height, bool fullscreen)
+	Application::Application(const std::string &title, Application::Flags flags, Application::ErrorFlags error_flags, unsigned int width, unsigned int height, bool fullscreen)
 : Framework::PropertyContainer<>()
 , window(nullptr)
+, error_flags(error_flags)
 
 , scene_system()
 , render_system()
@@ -40,64 +41,124 @@ Application::Application(const std::string &title, Application::Flags flags, uns
 , clean_up_sign()
 {
 
-	if (flags & GM_SCENE_SYSTEM)
+	if (flags & GM_FRAMEWORK_SCENE_SYSTEM)
 	{
 		scene_system = std::make_shared<Framework::SceneSystem>();
 	}
 
-	if (flags & GM_RENDER_SYSTEM)
+	if (flags & GM_FRAMEWORK_RENDER_SYSTEM)
 	{
 		render_system = std::make_shared<Framework::RenderSystem>();
 	}
 
-	if (flags & GM_ENTITY_MANAGER)
+	if (flags & GM_FRAMEWORK_ENTITY_MANAGER)
 	{
 		entity_manager = std::make_shared<Framework::EntityManager>();
 	}
 	
-	if (flags & GM_BUFFER_MANAGER)
+	if (flags & GM_FRAMEWORK_BUFFER_MANAGER)
 	{
 		buffer_manager = std::make_shared<Framework::BufferManager>();
 	}
 
-	if (flags & GM_SHADER_MANAGER)
+	if (flags & GM_FRAMEWORK_SHADER_MANAGER)
 	{
 		shader_manager = std::make_shared<Framework::ShaderManager>();
 	}
 	
-	if (flags & GM_TEXTURE_MANAGER)
+	if (flags & GM_FRAMEWORK_TEXTURE_MANAGER)
 	{
 		texture_manager = std::make_shared<Framework::TextureManager>(
 			std::make_shared<Framework::SoilTextureIO>()
 		);
 	}
 
-	if (flags & GM_VAO_MANAGER)
+	if (flags & GM_FRAMEWORK_VAO_MANAGER)
 	{
 		vao_manager = std::make_shared<Framework::VaoManager>();
 	}
 }
 
 void Application::run() {
-	if (!has_render_system()) {
-		throw std::runtime_error("Must have render system in order to run!");
+
+	//Test if we should check for null systems
+	if (error_flags & GM_ERROR_NULL_SYSTEM)
+	{
+		if (!has_scene_system()) {
+			throw std::runtime_error("Must have scene system in order to run!");
+		}
+
+		if (!has_render_system()) {
+			throw std::runtime_error("Must have render system in order to run!");
+		}
+
+		if (!has_entity_manager()) {
+			throw std::runtime_error("Must have entity manager in order to run!");
+		}
+
+		if (!has_buffer_manager()) {
+			throw std::runtime_error("Must have buffer manager in order to run!");
+		}
+
+		if (!has_shader_manager()) {
+			throw std::runtime_error("Must have shader manager in order to run!");
+		}
+
+		if (!has_texture_manager()) {
+			throw std::runtime_error("Must have texture manager in order to run!");
+		}
+
+		if (!has_vao_manager()) {
+			throw std::runtime_error("Must have vao manager in order to run!");
+		}
 	}
 
-	// set up window, context...
+	// TODO: set up window, context...
 	// TODO: check that context is valid
 
-	initialize_sign();
+	initialize();
 
-	while(keep_running)
+	while(is_running())
 	{
-		update_sign(0.1); // fix thiw with actual time
-		prepare_sign();
-		render_sign();
+		update();
+		prepare();
+		render();
 	}
 
-	clean_up_sign();
+	clean_up();
 
-	// destroy context, window
+	// TODO: destroy context, window
+}
+
+void Application::initialize() {
+	initialize_sign();
+}
+void Application::update() {
+
+	if (has_entity_manager()) {
+		entity_manager->update();
+	}
+
+	update_sign(0.1); // FIXME: fix this with actual time
+}
+void Application::prepare() {
+
+	if (has_scene_system()) {
+		scene_system->prepare();
+	}
+
+	prepare_sign();
+}
+void Application::render() {
+
+	if (has_render_system()) {
+		render_system->render();
+	}
+
+	render_sign();
+}
+void Application::clean_up() {
+	clean_up_sign();
 }
 
 } // namespace Application
