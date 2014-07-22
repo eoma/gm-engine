4d338d8a86157181afabd38fc096efc366e7d15e
Endre Oma <endre.88.oma@gmail.com>
2014-07-22 12:02:15 +0200

    Make ShaderManager use Core::Shader, Core::ShaderFactory

--- include/GM/Framework/Managers/ShaderManager.h
+++ include/GM/Framework/Managers/ShaderManager.h
@@ -9,6 +9,11 @@
 #include <vector>
 
 namespace GM {
+
+namespace Core {
+class Shader; typedef std::shared_ptr<Shader> ShaderPtr;
+}
+
 namespace Framework {
 
 class ShaderTemplateManager; typedef std::shared_ptr<ShaderTemplateManager> ShaderTemplateManagerPtr;
@@ -19,10 +24,10 @@ public:
 	ShaderManager();
 	~ShaderManager();
 
-	ShaderId get_or_create(const std::string &name);
-	ShaderId get_or_create(const std::string &name, const std::string &vs_file, const std::string &fs_file, bool rasterizer_discard);
-	ShaderId get_or_create(const std::string &name, const std::string &vs_file, const std::string &gs_file, const std::string &fs_file, bool rasterizer_discard);
-	ShaderId get_or_create(
+	Core::ShaderPtr get_or_create(const std::string &name);
+	Core::ShaderPtr get_or_create(const std::string &name, const std::string &vs_file, const std::string &fs_file, bool rasterizer_discard);
+	Core::ShaderPtr get_or_create(const std::string &name, const std::string &vs_file, const std::string &gs_file, const std::string &fs_file, bool rasterizer_discard);
+	Core::ShaderPtr get_or_create(
 		const std::string &name, const std::string &vs_file, const std::string &gs_file, const std::string &fs_file, 
 		const std::string &tess_ctrl_file, const std::string &tess_eval_file, const std::string &compute_file, bool rasterizer_discard);
 	// TODO: Extend to all 4.3 spec types (tesselation and compute is missing, support Transform Feedback with diabled rasterization, etc
@@ -38,14 +43,14 @@ private:
 	std::unordered_map<std::string, std::string> file_content_cache;
 
 	// Useful to map what files a specific shader is using.
-	std::unordered_map<ShaderId, std::set<std::string>> shader_to_file_deps;
+	std::unordered_map<Core::ShaderPtr, std::set<std::string>> shader_to_file_deps;
 
 	// Useful for mapping which shader pograms are using a file.
-	std::unordered_map<std::string, std::set<ShaderId>> file_to_shader_deps;
+	std::unordered_map<std::string, std::set<Core::ShaderPtr>> file_to_shader_deps;
 
 	//Associates a name with a shader.
-	std::unordered_map<std::string, ShaderId> name_to_shader;
-	std::unordered_map<ShaderId, std::string> shader_to_name;
+	std::unordered_map<std::string, Core::ShaderPtr> name_to_shader;
+	std::unordered_map<Core::ShaderPtr, std::string> shader_to_name;
 
 	ShaderTemplateManagerPtr template_manager;
 };
