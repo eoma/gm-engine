16874ac06cca9ad954a4181248d290583da2909c
Endre Oma <endre.88.oma@gmail.com>
2014-07-22 15:56:29 +0200

    Make Shader hold a vector of ShaderUniformInfo

--- include/GM/Core/GL/Shader.h
+++ include/GM/Core/GL/Shader.h
@@ -1,11 +1,24 @@
 #pragma once
 
+#include <GL/gl3w.h>
+
 #include <memory>
+#include <vector>
 
 namespace GM {
 namespace Core {
 
 class Shader; typedef std::shared_ptr<Shader> ShaderPtr;
+
+struct ShaderUniformInfo
+{
+	std::string name;
+	GLenum type;
+	unsigned int size; // How many multiples of type are there? Relevant for array
+	int location; // be default -1
+
+	ShaderUniformInfo(const std::string &name, const GLenum type, const unsigned int size, const int location = -1) : name(name), type(type), size(size), location(location) {};
+};
 
 class Shader
 {
@@ -16,10 +29,15 @@ public:
 	void bind() const;
 	void unbind() const;
 
-	unsigned int get_handle() const { return handle; };
+	unsigned int get_handle() const { return handle; }
+
+	const std::vector<ShaderUniformInfo> &get_uniform_info() const { return uniform_info; }
+	void set_uniform_info(const std::vector<ShaderUniformInfo> &new_uniform_info);
 
 private:
 	unsigned int handle;
+
+	std::vector<ShaderUniformInfo> uniform_info;
 };
 
 } // namespace Core
--- src/Core/GL/Shader.cpp
+++ src/Core/GL/Shader.cpp
@@ -25,5 +25,10 @@ void Shader::unbind() const
 	glUseProgram(0);
 }
 
+void Shader::set_uniform_info(const std::vector<ShaderUniformInfo> &new_uniform_info)
+{
+	uniform_info = new_uniform_info;
+}
+
 } // namespace Core
 } // namespace GM
