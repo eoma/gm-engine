d777d7c3867e1712f6e267608667bd8c7d1dff98
Endre Oma <endre.88.oma@gmail.com>
2014-07-22 16:37:55 +0200

    Update ShaderFactoryTest to fetch attribute information

--- tests/ShaderFactoryTest/main.cpp
+++ tests/ShaderFactoryTest/main.cpp
@@ -14,7 +14,9 @@ bool mainTest() {
 
 	app.set_gl_version(3, 3);
 
+	auto keep_running = app.add<bool>("keep_running", false);
 	bool update_called = false;
+
 	auto initialize_slot = app.on_initialize().connect([]() {
 		Core::ShaderPtr shader = nullptr;
 
@@ -23,6 +25,7 @@ bool mainTest() {
 		shader = Core::ShaderFactory::make_program({
 			Core::ShaderSource("vertex",
 				s("#version 330\n") +
+				s("in vec4 position_ws;\n") +
 				s("uniform int non_active_uniform = 0;\n") +
 				s("uniform int active_uniform = 1;\n") +
 				s("struct MyType {\n") + 
@@ -31,7 +34,7 @@ bool mainTest() {
 				s("};\n") +
 				s("uniform MyType my_type_collection[3];") +
 				s("void main() {\n") +
-				s("    gl_Position = vec4(active_uniform, 0.0, 0.0, 0.0);\n") +
+				s("    gl_Position = position_ws + vec4(active_uniform, 0.0, 0.0, 0.0);\n") +
 				s("    gl_Position.x = my_type_collection[1].some_float;\n") +
 				s("    for (int i = 0; i < 3; ++i) gl_Position.xyz += my_type_collection[i].something_array[0];\n") +
 				s("}\n")
@@ -50,6 +53,18 @@ bool mainTest() {
 				GL_FRAGMENT_SHADER),
 		});
 
+		for (const auto &info : shader->get_attribute_info())
+		{
+			std::cout << "Attribute: " << info.name << std::endl;
+			std::cout << "Type: " << std::hex << std::showbase << info.type << std::dec << std::endl;
+			std::cout << "Size: " << info.size << std::endl;
+			std::cout << "Location: " << info.location << std::endl;
+
+			std::cout << std::endl;
+		}
+
+		std::cout << "-----" << std::endl << std::endl;
+
 		for (const auto &info : shader->get_uniform_info())
 		{
 			std::cout << "Uniform: " << info.name << std::endl;
@@ -62,7 +77,7 @@ bool mainTest() {
 	});
 
 	auto update_slot = app.on_update().connect([&](float value) mutable {
-		app.stop_running();
+		keep_running = false;
 		update_called = true;
 	});
 
