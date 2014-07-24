dd301069eda561ec28d418358464d75bbfe2c8a8
Endre Oma <endre.88.oma@gmail.com>
2014-07-24 23:21:29 +0200

    Make buffer allocation implement an upload method

--- include/GM/Core/Utilities/BufferAllocation.h
+++ include/GM/Core/Utilities/BufferAllocation.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include "BufferOperations.h"
+#include "../GL/BufferObject.h"
+
 #include <memory>
+#include <vector>
 
 namespace GM {
 namespace Core {
@@ -12,6 +16,12 @@ struct BufferAllocation
 	BufferObjectPtr buffer;
 	unsigned int allocated_size;
 	unsigned int offset;
+
+	template <class... DataStructure>
+	void upload(const std::vector<DataStructure>&... data_structure_collections)
+	{
+		buffer->upload(allocated_size, offset, data_structure_collections...);
+	}
 };
 
 } // namespace Core
