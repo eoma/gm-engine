20e539d80e9701cfcdc353fc9759e04475840226
Endre Oma <endre.88.oma@gmail.com>
2014-07-28 11:31:24 +0200

    Add templated function total_size_plus_one

--- include/GM/Core/Utilities/Tools.h
+++ include/GM/Core/Utilities/Tools.h
@@ -13,15 +13,19 @@ namespace GM {
 		template <class... DataStructures>
 		size_t total_size(const DataStructures&... data_structures);
 
+		// Almost the same as total_size, except it will add one extra to each containers' size
+		template <class... DataStructures>
+		size_t total_size_plus_one(const DataStructures&... data_structures);
+
 		//
 		// Internal implementation declarations
 		//
 
 		template <class First, class... Rest>
-		size_t total_size_impl(size_t accumulated, const First &first, const Rest&... rest);
+		size_t total_size_impl(const size_t extra_count, size_t accumulated, const First &first, const Rest&... rest);
 
 		template <class Single>
-		size_t total_size_impl(size_t accumulated, const std::vector<Single> &single);
+		size_t total_size_impl(const size_t extra_count, size_t accumulated, const std::vector<Single> &single);
 
 		//
 		// Implementations
@@ -30,20 +34,28 @@ namespace GM {
 		template <class... datastructures>
 		size_t total_size(const datastructures&... data_structures)
 		{
-			return total_size_impl(0, data_structures...);
+			return total_size_impl(0, 0, data_structures...);
 		}
 
+		template <class... datastructures>
+		size_t total_size_plus_one(const datastructures&... data_structures)
+		{
+			return total_size_impl(1, 0, data_structures...);
+		}
 		
 		template <class First, class... Rest>
-		size_t total_size_impl(size_t accumulated, const First &first, const Rest&... rest)
+		size_t total_size_impl(const unsigned int extra_count, size_t accumulated, const First &first, const Rest&... rest)
 		{
-			return total_size_impl(total_size_impl(accumulated, first), rest...);
+			return total_size_impl(
+				extra_count,
+				total_size_impl(extra_count, accumulated, first),
+				rest...);
 		}
 
 		template <class Single>
-		size_t total_size_impl(size_t accumulated, const std::vector<Single> &single)
+		size_t total_size_impl(const size_t extra_count, size_t accumulated, const std::vector<Single> &single)
 		{
-			return accumulated + sizeof(Single)*single.size();
+			return accumulated + sizeof(Single)*(single.size()+extra_count);
 		}
 
 	} // namespace Core
