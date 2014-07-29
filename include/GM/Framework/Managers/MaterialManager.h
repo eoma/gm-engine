5ebdf5205cfb10f55efdbbf51bd2afcad5a614d8
Endre Oma <endre.88.oma@gmail.com>
2014-07-29 15:20:26 +0200

    Framework::MaterialManager: Add methods contains, get

--- include/GM/Framework/Managers/MaterialManager.h
+++ include/GM/Framework/Managers/MaterialManager.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <map>
 #include <memory>
+#include <string>
 
 namespace GM {
 namespace Framework {
@@ -12,9 +14,17 @@ public:
 	MaterialManager();
 	~MaterialManager();
 
+	bool contains(const std::string &name) const;
+
+	void add(const std::string& name, const MaterialPtr &material);
+
+	MaterialPtr get(const std::string& name) const;
+
+	//MaterialPtr get_or_create(const std::string& name);
 public:
 
 private:
+	std::map<std::string, MaterialPtr> materials;
 };
 
 typedef std::shared_ptr<MaterialManager> MaterialManagerPtr;
--- src/Framework/Managers/MaterialManager.cpp
+++ src/Framework/Managers/MaterialManager.cpp
@@ -1,6 +1,9 @@
 #include "GM/Framework/Managers/MaterialManager.h"
 
-using namespace GM::Framework;
+#include "ClanLib/core.h"
+
+namespace GM {
+namespace Framework {
 
 MaterialManager::MaterialManager() {
 
@@ -9,3 +12,39 @@ MaterialManager::MaterialManager() {
 MaterialManager::~MaterialManager() {
 
 }
+
+bool MaterialManager::contains(const std::string &name) const
+{
+	return (materials.end() !=  materials.find(name));
+}
+
+void MaterialManager::add(const std::string &name, const MaterialPtr &material)
+{
+	auto iter = materials.find(name);
+	if (iter == materials.end())
+	{
+		materials.insert(std::make_pair(name, material));
+	}
+	else
+	{
+		clan::StringFormat message = clan::StringFormat("Material manager already contains a material by name (%1)");
+		message.set_arg(1, name);
+		throw clan::Exception(message.get_result());
+	}
+}
+
+MaterialPtr MaterialManager::get(const std::string &name) const
+{
+	MaterialPtr material = nullptr;
+
+	auto iter = materials.find(name);
+	if (iter != materials.end())
+	{
+		material = iter->second;
+	}
+
+	return material;
+}
+
+} // namespace Framework
+} // namespace GM
