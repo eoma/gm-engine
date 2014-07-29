aff5f6b7af79545bb6c5845f879511a4f8d45d1a
Endre Oma <endre.88.oma@gmail.com>
2014-07-29 15:30:16 +0200

    Make Framework::Renderable implement revised Framework::IRenderable

--- include/GM/Framework/Components/Renderable.h
+++ include/GM/Framework/Components/Renderable.h
@@ -11,30 +11,43 @@
 namespace GM {
 namespace Framework {
 
+class MeshManager; typedef std::shared_ptr<MeshManager> MeshManagerPtr;
+class MaterialManager; typedef std::shared_ptr<MaterialManager> MaterialManagerPtr;
+
 class Renderable; typedef std::shared_ptr<Renderable> RenderablePtr;
 class RenderSystem; typedef std::shared_ptr<RenderSystem> RenderSystemPtr;
 
 class Renderable : public IRenderable, public Component<Renderable> {
 public:
-	Renderable(const EntityPtr &owner, const RenderSystemPtr &render_system, unsigned int render_layers = 1, const std::string &name = std::string());
+	Renderable(const EntityPtr &owner, const RenderSystemPtr &render_system, const MaterialManagerPtr material_manager, const MeshManagerPtr mesh_ptr, unsigned int render_layers = 1, const std::string &name = std::string());
 	virtual ~Renderable();
 
 	std::string get_type() const override { return get_static_type(); }
 
-	virtual	glm::mat4 get_world_matrix() const override { return world_matrix_property; };
-	virtual	glm::mat4 get_object_matrix() const override { return object_matrix_property; };
+	virtual	glm::mat4 get_world_matrix() const override { return world_matrix_property; }
+	virtual	glm::mat4 get_object_matrix() const override { return object_matrix_property; }
 
-	virtual bool is_culled() const override { return culled_property; };
-	virtual bool is_invisible() const override { return !visible_property || is_culled(); };
+	virtual bool is_culled() const override { return culled_property; }
+	virtual bool is_invisible() const override { return !visible_property || is_culled(); }
 
-	virtual unsigned int get_render_layers() const override { return render_layers; };
+	virtual unsigned int get_render_layers() const override { return render_layers; }
 
-	virtual void render(Camera *camera) override;
+	virtual const MaterialPtr &get_material() const override { return nullptr; }
+
+	virtual const MeshPtr &get_mesh() const override { return nullptr; }
+
+	virtual bool has_custom_render() const override { return false; }
+
+	virtual void custom_render(Camera *camera) override {};
 public:
 	static std::string get_static_type() { return COMPONENT_RENDERABLE; }
 
 private:
 	RenderSystemPtr render_system;
+
+	MaterialManagerPtr material_manager;
+	MeshManagerPtr mesh_manager;
+
 	const unsigned int render_layers;
 
 	Property<glm::mat4> world_matrix_property;
@@ -45,6 +58,11 @@ private:
 
 	Property<std::string> material_name_property;
 	Property<std::string> mesh_name_property;
+
+	clan::SlotContainer slots;
+
+	MaterialPtr material;
+	MeshPtr mesh;
 };
 
 } // namespace Framework
--- src/Framework/Components/Renderable.cpp
+++ src/Framework/Components/Renderable.cpp
@@ -2,13 +2,18 @@
 #include "GM/Framework/Components/Renderable.h"
 #include "GM/Framework/DefinitionsPropertyNames.h"
 #include "GM/Framework/Entity.h"
+#include "GM/Framework/Managers/MaterialManager.h"
+#include "GM/Framework/Managers/MeshManager.h"
 #include "GM/Framework/Systems/RenderSystem.h"
+#include "GM/Core/GL/Render.h"
 
 using namespace GM::Framework;
 
-Renderable::Renderable(const EntityPtr &owner, const RenderSystemPtr &render_system, const unsigned int render_layers, const std::string &name)
+Renderable::Renderable(const EntityPtr &owner, const RenderSystemPtr &render_system, const MaterialManagerPtr material_manager, const MeshManagerPtr mesh_manager, const unsigned int render_layers, const std::string &name)
 : Component<Renderable>(owner, name)
 , render_system(render_system)
+, material_manager(material_manager)
+, mesh_manager(mesh_manager)
 , render_layers(render_layers)
 {
 	render_system->add_renderable(this);
@@ -20,13 +25,34 @@ Renderable::Renderable(const EntityPtr &owner, const RenderSystemPtr &render_sys
 	visible_property = owner->add(PROPERTY_VISIBLE, false);
 
 	material_name_property = owner->add(PROPERTY_MATERIAL_NAME, std::string());
+	slots.connect(material_name_property.value_changed(),
+		[&](const std::string &/*old_material_name*/, const std::string &new_material_name) mutable
+		{
+			if (new_material_name.empty())
+			{
+				material = nullptr;
+			}
+			else
+			{
+				material = material_manager->get(new_material_name);
+			}
+		});
+
 	mesh_name_property = owner->add(PROPERTY_MESH_NAME, std::string());
+	slots.connect(mesh_name_property.value_changed(),
+		[&](const std::string &/*old_mesh_name*/, const std::string &new_mesh_name) mutable
+		{
+			if (new_mesh_name.empty())
+			{
+				mesh = nullptr;
+			}
+			else
+			{
+				mesh = mesh_manager->get(new_mesh_name);
+			}
+		});
 }
 
 Renderable::~Renderable() {
 	render_system->remove_renderable(this);
-}
-
-void Renderable::render(Camera * /*camera*/) {
-
 }
