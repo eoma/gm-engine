efec4f5ad35b68710a5a3b29ddcaa35b0019272f
Endre Oma <endre.88.oma@gmail.com>
2014-07-29 15:19:00 +0200

    Framework::IRenderable: Add methods get_{mesh,material}, rename render -> custom_render

--- include/GM/Framework/Components/IRenderable.h
+++ include/GM/Framework/Components/IRenderable.h
@@ -8,6 +8,8 @@ namespace GM {
 namespace Framework {
 
 class Camera;
+class Material; typedef std::shared_ptr<Material> MaterialPtr;
+class Mesh; typedef std::shared_ptr<Mesh> MeshPtr;
 class IRenderable; typedef std::shared_ptr<IRenderable> IRenderablePtr;
 
 class IRenderable {
@@ -29,7 +31,20 @@ public:
 	// Default should be 1.
 	virtual unsigned int get_render_layers() const = 0;
 
-	virtual void render(Camera *camera) = 0;
+	// This should not change while in a render system.
+	// If you want to change it, remove yourself from the render system,
+	// change it and add yourself again.
+	virtual bool has_custom_render() const = 0;
+
+	//  Will be called if has_custom_render() returns true
+	virtual void custom_render(Camera *camera) = 0;
+
+	// The following are relevant in sorting and subsequently in constructing
+	// a draw command buffer
+
+	virtual const MaterialPtr &get_material() const = 0;
+
+	virtual const MeshPtr &get_mesh() const = 0;
 };
 
 } // namespace Framework
