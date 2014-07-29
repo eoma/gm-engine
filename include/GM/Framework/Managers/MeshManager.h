51e09da810b374e9559811d2b5bd30158e5f0e2b
Endre Oma <endre.88.oma@gmail.com>
2014-07-29 15:21:32 +0200

    Framework::MeshManager: Add methods contains(), get() and add()

--- include/GM/Framework/Managers/MeshManager.h
+++ include/GM/Framework/Managers/MeshManager.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <map>
 #include <memory>
+#include <string>
 
 namespace GM {
 	namespace Framework {
@@ -12,9 +14,17 @@ namespace GM {
 			MeshManager();
 			~MeshManager();
 
+			bool contains(const std::string &name) const;
+
+			void add(const std::string &name, const MeshPtr &mesh);
+
+			MeshPtr get(const std::string &name) const;
+
+			//MeshPtr get_or_create(const std::string &name);
 		public:
 
 		private:
+			std::map<std::string, MeshPtr> meshes;
 		};
 
 		typedef std::shared_ptr<MeshManager> MeshManagerPtr;
--- src/Framework/Managers/MeshManager.cpp
+++ src/Framework/Managers/MeshManager.cpp
@@ -1,8 +1,11 @@
 #include "GM/Framework/Managers/MeshManager.h"
 
+#include "ClanLib/core.h"
+
 #include <algorithm>
 
-using namespace GM::Framework;
+namespace GM {
+namespace Framework {
 
 MeshManager::MeshManager() {
 
@@ -11,3 +14,38 @@ MeshManager::MeshManager() {
 MeshManager::~MeshManager() {
 
 }
+
+bool MeshManager::contains(const std::string &name) const
+{
+	return (meshes.end() != meshes.find(name));
+}
+
+void MeshManager::add(const std::string &name, const MeshPtr &mesh)
+{
+	if (!contains(name))
+	{
+		meshes.insert(std::make_pair(name, mesh));
+	}
+	else
+	{
+		clan::StringFormat message = clan::StringFormat("Mesh manager already contains a mesh by name (%1)");
+		message.set_arg(1, name);
+		throw clan::Exception(message.get_result());
+	}
+}
+
+MeshPtr MeshManager::get(const std::string &name) const
+{
+	MeshPtr mesh = nullptr;
+
+	auto iter = meshes.find(name);
+	if (iter != meshes.end())
+	{
+		mesh = iter->second;
+	}
+
+	return mesh;
+}
+
+} // namespace Framework
+} // namespace GM
