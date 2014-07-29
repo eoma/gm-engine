844f599e855f0ea060f3d9c050d9a8ac07624d20
Endre Oma <endre.88.oma@gmail.com>
2014-07-29 15:25:26 +0200

    Add Core::RenderCommand to Framework::Mesh

--- include/GM/Framework/Utilities/Mesh.h
+++ include/GM/Framework/Utilities/Mesh.h
@@ -2,6 +2,7 @@
 
 #include "../Managers/VaoManager.h"
 #include <GM/Core/Utilities/VaoLayout.h>
+#include <GM/Core/Utilities/RenderCommand.h>
 
 #include <memory>
 
@@ -14,15 +15,17 @@ namespace Framework {
 class Mesh
 {
 public:
-	Mesh(const std::string &name, const Core::VaoLayout &vao_layout, const VaoManagerPtr &vao_manager);
+	Mesh(const std::string &name, const Core::RenderCommand& render_command, const Core::VaoLayout &vao_layout, const VaoManagerPtr &vao_manager);
 	virtual ~Mesh() {};
 
 	const std::string &get_name() const { return name; }
 	const Core::VertexArrayObjectPtr &get_vao() const { return vao; }
+	const Core::RenderCommand &get_render_command() const { return render_command; }
 
 private:
 	std::string name;
 	Core::VertexArrayObjectPtr vao;
+	Core::RenderCommand render_command;
 };
 
 } // namespace Framework
--- src/Framework/Utilities/Mesh.cpp
+++ src/Framework/Utilities/Mesh.cpp
@@ -3,8 +3,9 @@
 namespace GM {
 namespace Framework {
 
-	Mesh::Mesh(const std::string &name, const Core::VaoLayout &vao_layout, const VaoManagerPtr &vao_manager)
+	Mesh::Mesh(const std::string &name, const Core::RenderCommand &render_command, const Core::VaoLayout &vao_layout, const VaoManagerPtr &vao_manager)
 		: name(name)
+		, render_command(render_command)
 {
 		vao = vao_manager->get_vao_for(vao_layout);
 }
