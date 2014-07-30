97d977913eaddd8ff9734c0a7b63c579c3edc9c4
Pål Trefall <admin@ptrefall.com>
2014-07-30 19:58:16 +0200

    Step by step on AssimpMeshIO.

--- src/Framework/IO/AssimpMeshIO.cpp
+++ src/Framework/IO/AssimpMeshIO.cpp
@@ -1,6 +1,7 @@
 #include "GM/Framework/IO/AssimpMeshIO.h"
 #include <assimp/Importer.hpp>
 #include <assimp/postprocess.h>
+#include <assimp/scene.h>
 #include <ClanLib/core.h>
 
 #include <cctype>
@@ -22,6 +23,9 @@ AssimpMeshIO::~AssimpMeshIO()
 Core::VaoLayout AssimpMeshIO::load(const std::string &file_name)
 {
 	auto scene = importer->ReadFile(file_name, aiProcessPreset_TargetRealtime_MaxQuality);
+	auto mesh_index = scene->mRootNode->mMeshes[0];
+	auto mesh = scene->mMeshes[mesh_index];
+	
 	//clan::DataBuffer buffer = clan::File::read_bytes(file_name);
 	//return load(buffer.get_data<unsigned char>(), buffer.get_size());
 	return Core::VaoLayout();
