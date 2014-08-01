aeb237c9eae847980430b06a46030a0cda5b17e5
Endre Oma <endre.88.oma@gmail.com>
2014-08-01 18:01:43 +0200

    Update shaders and material descriptions for TriangleTestJson

--- resources/glsl/samples/triangle/diffuse.fs
+++ resources/glsl/samples/triangle/diffuse.fs
@@ -1,6 +1,7 @@
 #version 330 
 // TODO: Rewrite to accept a uniform color input here instead of setting to red.
 out vec3 color;
+uniform vec3 diffuse = vec3(0,0,0);
 void main() {
-	color = vec3(1,0,0);
+	color = diffuse;
 }
