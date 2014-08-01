dea3ce5de258f119fed407f4f24320f6d90c4647
Endre Oma <endre.88.oma@gmail.com>
2014-08-01 08:31:01 +0200

    Add Core::UniformOperations

---
+++ include/GM/Core/Utilities/UniformOperations.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+#include <memory>
+
+namespace GM {
+namespace Core {
+
+// Primitives
+void update_uniform(unsigned int program, int location, char data);
+void update_uniform(unsigned int program, int location, unsigned char data);
+
+void update_uniform(unsigned int program, int location, short data);
+void update_uniform(unsigned int program, int location, unsigned short data);
+
+void update_uniform(unsigned int program, int location, int data);
+void update_uniform(unsigned int program, int location, unsigned int data);
+
+void update_uniform(unsigned int program, int location, float data);
+
+// Vectors
+void update_uniform(unsigned int program, int location, const glm::vec2& data);
+void update_uniform(unsigned int program, int location, const glm::ivec2& data);
+void update_uniform(unsigned int program, int location, const glm::uvec2& data);
+
+void update_uniform(unsigned int program, int location, const glm::vec3& data);
+void update_uniform(unsigned int program, int location, const glm::ivec3& data);
+void update_uniform(unsigned int program, int location, const glm::uvec3& data);
+
+void update_uniform(unsigned int program, int location, const glm::vec4& data);
+void update_uniform(unsigned int program, int location, const glm::ivec4& data);
+void update_uniform(unsigned int program, int location, const glm::uvec4& data);
+
+// Matrices
+void update_uniform(unsigned int program, int location, const glm::mat2& data);
+void update_uniform(unsigned int program, int location, const glm::mat3& data);
+void update_uniform(unsigned int program, int location, const glm::mat4& data);
+
+// Textures
+
+
+} // namespace Core
+} // namespace GM
---
+++ src/Core/Utilities/UniformOperations.cpp
@@ -0,0 +1,116 @@
+#include "GM/Core/Utilities/UniformOperations.h"
+
+#include <glm/ext.hpp>
+
+#include <GL/gl3w.h>
+
+namespace GM {
+namespace Core {
+
+//
+// Primitives
+//
+void update_uniform(unsigned int program, int location, char data)
+{
+	glProgramUniform1i(program, location, data);
+}
+
+void update_uniform(unsigned int program, int location, unsigned char data)
+{
+	glProgramUniform1ui(program, location, data);
+}
+
+void update_uniform(unsigned int program, int location, short data)
+{
+	glProgramUniform1i(program, location, data);
+}
+
+void update_uniform(unsigned int program, int location, unsigned short data)
+{
+	glProgramUniform1ui(program, location, data);
+}
+
+void update_uniform(unsigned int program, int location, int data)
+{
+	glProgramUniform1i(program, location, data);
+}
+
+void update_uniform(unsigned int program, int location, unsigned int data)
+{
+	glProgramUniform1ui(program, location, data);
+}
+
+void update_uniform(unsigned int program, int location, float data)
+{
+	glProgramUniform1f(program, location, data);
+}
+
+//
+// Vectors
+//
+void update_uniform(unsigned int program, int location, const glm::vec2 &data)
+{
+	glProgramUniform2fv(program, location, 1, glm::value_ptr(data));
+}
+
+void update_uniform(unsigned int program, int location, const glm::ivec2 &data)
+{
+	glProgramUniform2iv(program, location, 1, glm::value_ptr(data));
+}
+
+void update_uniform(unsigned int program, int location, const glm::uvec2 &data)
+{
+	glProgramUniform2uiv(program, location, 1, glm::value_ptr(data));
+}
+
+void update_uniform(unsigned int program, int location, const glm::vec3 &data)
+{
+	glProgramUniform3fv(program, location, 1, glm::value_ptr(data));
+}
+
+void update_uniform(unsigned int program, int location, const glm::ivec3 &data)
+{
+	glProgramUniform3iv(program, location, 1, glm::value_ptr(data));
+}
+
+void update_uniform(unsigned int program, int location, const glm::uvec3 &data)
+{
+	glProgramUniform3uiv(program, location, 1, glm::value_ptr(data));
+}
+
+void update_uniform(unsigned int program, int location, const glm::vec4 &data)
+{
+	glProgramUniform4fv(program, location, 1, glm::value_ptr(data));
+}
+
+void update_uniform(unsigned int program, int location, const glm::ivec4 &data)
+{
+	glProgramUniform4iv(program, location, 1, glm::value_ptr(data));
+}
+
+void update_uniform(unsigned int program, int location, const glm::uvec4 &data)
+{
+	glProgramUniform4uiv(program, location, 1,  glm::value_ptr(data));
+}
+
+//
+// Matrices
+//
+
+void update_uniform(unsigned int program, int location, const glm::mat2 &data)
+{
+	glProgramUniformMatrix2fv(program, location, 1, false, glm::value_ptr(data));
+}
+
+void update_uniform(unsigned int program, int location, const glm::mat3 &data)
+{
+	glProgramUniformMatrix3fv(program, location, 1, false, glm::value_ptr(data));
+}
+
+void update_uniform(unsigned int program, int location, const glm::mat4 &data)
+{
+	glProgramUniformMatrix4fv(program, location, 1, false, glm::value_ptr(data));
+}
+
+} // namespace Core
+} // namespace GM
