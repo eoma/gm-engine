57387f12a95e0cb1a19fe39536e8b0a0e3ebe384
Endre Oma <endre.88.oma@gmail.com>
2014-08-03 12:38:41 +0200

    Implement Core::TextureFactory

--- include/GM/Core/Utilities/TextureFactory.h
+++ include/GM/Core/Utilities/TextureFactory.h
@@ -2,6 +2,8 @@
 
 #include "TextureFormat.h"
 
+#include <GL/gl3w.h>
+
 #include <functional>
 #include <memory>
 #include <string>
@@ -10,27 +12,38 @@
 namespace GM {
 namespace Core {
 
+class Texture; typedef std::shared_ptr<Texture> TexturePtr;
+
 class TextureFactory
 {
 public:
-	typedef std::function<std::shared_ptr<const std::vector<unsigned char>> (const std::string &file_name, unsigned int *width, unsigned int *height, unsigned int *num_channels)> FileFetcherFunction;
+	typedef std::function<std::shared_ptr<const std::vector<unsigned char>> (const std::string &file_name, int &width, int &height, GLenum &texture_format, GLenum &type)> FileFetcherFunction;
 
 	/** Creates and loads a texture as the format requires.
 	 * You _must_ specify a file fetcher. The function do not care how the image is produced, as long as it is produced.
 	 *
 	 * The file fetcher can be whatever, eg.
-	 * [](const std::string &name, unsigned int *w, unsigned int *h, unsigned int *c) -> std::shared_ptr<const vector<unsigned char>> {
-	 * 	SOILTextureIO io; 
-	 * 	auto img = io.load(name); 
-	 * 	*w = img.get_width(); 
-	 * 	*h = img.get_height(); 
-	 * 	*c = img.get_num_channels; 
-	 * 	return tmg.get_dataptr(); 
+	 * [](const std::string &name, int &w, int &h, GLenum &gl_texture_format, GLenum &gl_type) -> std::shared_ptr<const vector<unsigned char>> {
+	 * 	SOILTextureIO io;
+	 * 	auto img = io.load(name);
+	 * 	w = img.get_width();
+	 * 	h = img.get_height();
+	 * 	int channels = img.get_num_channels();
+	 *      if (channels = 1) gl_texture_format = GL_RED;
+	 *      if (channels = 2) gl_texture_format = GL_RG;
+	 *      if (channels = 3) gl_texture_format = GL_RGB;
+	 *      if (channels = 4) gl_texture_format = GL_RGBA;
+	 *      gl_type = GL_UNSIGNED_BYTE;
+	 * 	return img.get_dataptr();
 	 * };
 	 *
-	 * @return A texture id
+	 * @return A shared pointer of a Core::Texture instance
 	 */
-	static unsigned int create(const TextureFormat &format, const std::vector<std::string> &image_paths, FileFetcherFunction file_fetcher);
+	static TexturePtr create(const TextureFormat &format, const std::vector<std::string> &image_paths, FileFetcherFunction &file_fetcher);
+
+private:
+	static void upload(const TexturePtr& texture, const std::vector<std::string> &image_paths, FileFetcherFunction &file_fetcher);
+	static void upload_single_image(const GLenum target, const std::string &file_name, FileFetcherFunction &file_fetcher);
 
 };
 
---
+++ src/Core/Utilities/TextureFactory.cpp
@@ -0,0 +1,102 @@
+#include "GM/Core/Utilities/TextureFactory.h"
+
+#include "GM/Core/GL/Texture.h"
+
+#include "ClanLib/core.h"
+
+#include <sstream>
+
+namespace GM {
+namespace Core {
+
+TexturePtr TextureFactory::create(const TextureFormat &format, const std::vector<std::string> &image_paths, FileFetcherFunction &file_fetcher)
+{
+	auto texture = std::make_shared<Texture>(format.get_type());
+	texture->bind();
+
+	upload(texture, image_paths, file_fetcher);
+
+	for (const ITextureParameterPtr &param : format.get_parameters())
+	{
+		if (ITextureParameter::is_type<int>(param))
+		{
+			auto int_param = std::static_pointer_cast<TextureParameter<int>>(param);
+			glTexParameteri(format.get_type(), param->get_name(), int_param->get_param());
+		}
+		else if (ITextureParameter::is_type<float>(param))
+		{
+			auto float_param = std::static_pointer_cast<TextureParameter<float>>(param);
+			glTexParameterf(format.get_type(), param->get_name(), float_param->get_param());
+		}
+		else if (ITextureParameter::is_type<std::vector<int>>(param))
+		{
+			// Should perform checks on the required number of parameters
+			auto int_params = std::static_pointer_cast<TextureParameter<std::vector<int>>>(param);
+			glTexParameteriv(format.get_type(), param->get_name(), int_params->get_param().data());
+		}
+		else if (ITextureParameter::is_type<std::vector<float>>(param))
+		{
+			// Should perform checks on the required number of parameters
+			auto float_params = std::static_pointer_cast<TextureParameter<std::vector<float>>>(param);
+			glTexParameterfv(format.get_type(), param->get_name(), float_params->get_param().data());
+		}
+	}
+
+	if (format.is_generating_mipmap())
+	{
+		glGenerateMipmap(format.get_type());
+	}
+
+	texture->unbind();
+
+	return texture;
+}
+
+void TextureFactory::upload(const TexturePtr &texture, const std::vector<std::string> &files, FileFetcherFunction &file_fetcher)
+{
+	if (texture->get_type() == GL_TEXTURE_2D)
+	{
+		if (files.size() != 1)
+		{
+			throw clan::Exception(clan::string_format("A 2d texture requires 1 image, (%1) were provided", files.size()));
+		}
+
+		upload_single_image(GL_TEXTURE_2D, files[0], file_fetcher);
+	}
+	else if (texture->get_type() == GL_TEXTURE_CUBE_MAP)
+	{
+		if (files.size() != 6)
+		{
+			throw clan::Exception(clan::string_format("A cube texture requires 6 images, (%1) were provided", files.size()));
+		}
+
+		for (unsigned int i = 0; i < 6; ++i)
+		{
+			upload_single_image(GL_TEXTURE_CUBE_MAP_POSITIVE_X+i, files[i], file_fetcher);
+		}
+	}
+	else
+	{
+		std::stringstream str;
+		str << "Unhandled texture type 0x" << std::hex << texture->get_type() << std::dec;
+		throw clan::Exception(str.str());
+	}
+}
+
+void TextureFactory::upload_single_image(const GLenum target, const std::string &name, FileFetcherFunction &file_fetcher)
+{
+	int width = 0;
+	int height = 0;
+	GLenum texture_format = 0;
+	GLenum type = 0;
+
+	std::shared_ptr<const std::vector<unsigned char>> imgdata = file_fetcher(name, width, height, 
+		texture_format, type);
+
+	glTexImage2D(target, 
+	0, texture_format, width, height, 
+	0, texture_format, type, imgdata->data());
+}
+
+} // namespace Core
+} // namespace GM
