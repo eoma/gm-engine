929d4b39d436ba6b382a5d8dd207ddf47f6c94e1
Endre Oma <endre.88.oma@gmail.com>
2014-08-04 14:26:47 +0200

    Core::FramebufferObject: Enable some commented out functionality

--- include/GM/Core/GL/FramebufferObject.h
+++ include/GM/Core/GL/FramebufferObject.h
@@ -2,6 +2,8 @@
 
 #include <GL/gl3w.h>
 
+#include <glm/glm.hpp>
+
 #include <memory>
 #include <vector>
 
@@ -9,6 +11,7 @@ namespace GM {
 namespace Core {
 
 class RenderbufferObject; typedef std::shared_ptr<RenderbufferObject> RenderbufferObjectPtr;
+class Shader; typedef std::shared_ptr<Shader> ShaderPtr;
 class Texture; typedef std::shared_ptr<Texture> TexturePtr;
 
 class FramebufferObject
@@ -20,14 +23,14 @@ public:
 	void bind();
 	void unbind();
 
-	//void bind_rt(unsigned int active_program, unsigned int index_offset);
-	//void unbind_rt(unsigned int index_offset);
+	void bind_rt(const ShaderPtr &active_program, unsigned int index_offset);
+	void unbind_rt(unsigned int index_offset);
 
 	void add(unsigned int attachment, const RenderbufferObjectPtr &render_buffer);
-	//void add(unsigned int attachment, unsigned int texture_type, const std::string &sampler_name, const TexturePtr &render_texture);
+	void add(unsigned int attachment, unsigned int texture_type, const std::string &sampler_name, const TexturePtr &render_texture);
 
 	void check();
-	//glm::vec4 pick(int x, int y, unsigned int attachment);
+	glm::vec4 pick(int x, int y, unsigned int attachment);
 
 	TexturePtr get_render_texture(unsigned int index) const { return render_textures[index]; }
 
@@ -41,7 +44,7 @@ private:
 
 	std::vector<RenderbufferObjectPtr> render_buffers;
 	std::vector<TexturePtr> render_textures;
-	//std::vector<UniformPtr> render_samplers;
+	std::vector<std::string> render_samplers;
 	std::vector<unsigned int> attachments;
 };
 
