9f13b8e2deddb00fbf7793a89a0607e9655755d6
Pål Trefall <admin@ptrefall.com>
2014-08-15 09:18:09 +0200

    Finished the robot sample.

--- samples/RobotSampleJson/main.cpp
+++ samples/RobotSampleJson/main.cpp
@@ -13,8 +13,11 @@
 using namespace GM;
 using namespace Application;
 
+// We use this index to easily iterate through the different robot parts
+// using + and - for input handling.
 int currently_selected_part;
 
+// Define all our robot parts as a simple enum for simple iteration and lookup
 enum RobotParts {
 	ROBOT = 0,
 	BASE_LEFT,
@@ -29,6 +32,7 @@ enum RobotParts {
 	ROBOT_PARTS_COUNT
 };
 
+// Convert the robot part to a name
 std::string to_robot_part_name(RobotParts robot_part) {
 	switch (robot_part) {
 	case ROBOT:
@@ -56,6 +60,65 @@ std::string to_robot_part_name(RobotParts robot_part) {
 	};
 }
 
+// Make selection jumps so that we jump between specific behaviors
+// based on our input handling.
+int selection_jump_up(RobotParts robot_part) {
+	switch (robot_part) {
+	case ROBOT:
+		return 1;
+	case BASE_LEFT:
+		return 3;
+	case BASE_RIGHT:
+		return 2;
+	case ARM_LOWER:
+		return 1;
+	case ARM_UPPER:
+		return 1;
+	case HEAD:
+		return 3;
+	case CLAW_LOWER_LEFT:
+		return 2;
+	case CLAW_LOWER_RIGHT:
+		return 1;
+	case CLAW_UPPER_LEFT:
+		return 2;
+	case CLAW_UPPER_RIGHT:
+		return 1;
+	default:
+		return 1;
+	};
+}
+
+// Make selection jumps so that we jump between specific behaviors
+// based on our input handling.
+int selection_jump_down(RobotParts robot_part) {
+	switch (robot_part) {
+	case ROBOT:
+		return 1;
+	case BASE_LEFT:
+		return 1;
+	case BASE_RIGHT:
+		return 2;
+	case ARM_LOWER:
+		return 3;
+	case ARM_UPPER:
+		return 1;
+	case HEAD:
+		return 1;
+	case CLAW_LOWER_LEFT:
+		return 2;
+	case CLAW_LOWER_RIGHT:
+		return 3;
+	case CLAW_UPPER_LEFT:
+		return 1;
+	case CLAW_UPPER_RIGHT:
+		return 2;
+	default:
+		return 1;
+	};
+}
+
+// Find a specific robot part in the robot scenegraph hierarchy
 Framework::Transform *find_part(Framework::Transform *robot_or_part, RobotParts robot_part) {
 	std::string part_name = to_robot_part_name(robot_part);
 	if (part_name == robot_or_part->get_owner()->get_name())
@@ -75,7 +138,9 @@ Framework::Transform *find_part(Framework::Transform *robot_or_part, RobotParts 
 	return nullptr;
 }
 
+// Create and assemble the robot
 Framework::TransformPtr create_robot(Framework::EntityManagerPtr entity_manager) {
+
 	// Create empty entities
 	auto robot_entity = entity_manager->create_entity(					to_robot_part_name(RobotParts::ROBOT) );
 	auto robot_base_left_entity = entity_manager->create_entity(		to_robot_part_name(RobotParts::BASE_LEFT));
@@ -83,39 +148,55 @@ Framework::TransformPtr create_robot(Framework::EntityManagerPtr entity_manager)
 	auto robot_arm_lower_entity = entity_manager->create_entity(		to_robot_part_name(RobotParts::ARM_LOWER));
 	auto robot_arm_upper_entity = entity_manager->create_entity(		to_robot_part_name(RobotParts::ARM_UPPER));
 	auto robot_head_entity = entity_manager->create_entity(				to_robot_part_name(RobotParts::HEAD));
-	auto robot_claw_upper_left_entity = entity_manager->create_entity(	to_robot_part_name(RobotParts::CLAW_UPPER_LEFT));
-	auto robot_claw_upper_right_entity = entity_manager->create_entity(	to_robot_part_name(RobotParts::CLAW_UPPER_RIGHT));
 	auto robot_claw_lower_left_entity = entity_manager->create_entity(	to_robot_part_name(RobotParts::CLAW_LOWER_LEFT));
 	auto robot_claw_lower_right_entity = entity_manager->create_entity(	to_robot_part_name(RobotParts::CLAW_LOWER_RIGHT));
+	auto robot_claw_upper_left_entity = entity_manager->create_entity(to_robot_part_name(RobotParts::CLAW_UPPER_LEFT));
+	auto robot_claw_upper_right_entity = entity_manager->create_entity(to_robot_part_name(RobotParts::CLAW_UPPER_RIGHT));
 
-	// Apply templates
+	// Apply templates from entity_templates.json, basically adding components to the entities and setting property values
 	entity_manager->apply("robot", robot_entity);
 	entity_manager->apply("robot_base", robot_base_left_entity);
 	entity_manager->apply("robot_base", robot_base_right_entity);
 	entity_manager->apply("robot_arm", robot_arm_lower_entity);
 	entity_manager->apply("robot_arm", robot_arm_upper_entity);
 	entity_manager->apply("robot_head", robot_head_entity);
+	entity_manager->apply("robot_claw", robot_claw_lower_left_entity);
+	entity_manager->apply("robot_claw", robot_claw_lower_right_entity);
+	entity_manager->apply("robot_claw", robot_claw_upper_left_entity);
+	entity_manager->apply("robot_claw", robot_claw_upper_right_entity);
 
 	// Extract scenegraph components (Transform)
-	auto robot = robot_entity->get_component<Framework::Transform>();
-	auto robot_base_left = robot_base_left_entity->get_component<Framework::Transform>();
-	auto robot_base_right = robot_base_right_entity->get_component<Framework::Transform>();
-	auto robot_arm_lower = robot_arm_lower_entity->get_component<Framework::Transform>();
-	auto robot_arm_upper = robot_arm_upper_entity->get_component<Framework::Transform>();
-	auto robot_head = robot_head_entity->get_component<Framework::Transform>();
+	auto robot					= robot_entity->get_component<Framework::Transform>();
+	auto robot_base_left		= robot_base_left_entity->get_component<Framework::Transform>();
+	auto robot_base_right		= robot_base_right_entity->get_component<Framework::Transform>();
+	auto robot_arm_lower		= robot_arm_lower_entity->get_component<Framework::Transform>();
+	auto robot_arm_upper		= robot_arm_upper_entity->get_component<Framework::Transform>();
+	auto robot_head				= robot_head_entity->get_component<Framework::Transform>();
+	auto robot_claw_lower_left	= robot_claw_lower_left_entity->get_component<Framework::Transform>();
+	auto robot_claw_lower_right = robot_claw_lower_right_entity->get_component<Framework::Transform>();
+	auto robot_claw_upper_left	= robot_claw_upper_left_entity->get_component<Framework::Transform>();
+	auto robot_claw_upper_right = robot_claw_upper_right_entity->get_component<Framework::Transform>();
 
-	// Assemble the robot
+	// Assemble the robot (set up the scenegraph hierarchy)
 	robot->add_child(robot_base_left);
 	robot->add_child(robot_base_right);
 	robot->add_child(robot_arm_lower);
 	robot_arm_lower->add_child(robot_arm_upper);
 	robot_arm_upper->add_child(robot_head);
+	robot_head->add_child(robot_claw_lower_left);
+	robot_head->add_child(robot_claw_lower_right);
+	robot_claw_lower_left->add_child(robot_claw_upper_left);
+	robot_claw_lower_right->add_child(robot_claw_upper_right);
 
-	// Position assembled robot parts
+	// Position assembled robot parts (relative to their parent in the scenegraph)
 	robot_base_left->translate(glm::vec3(-1, 0, 0));
 	robot_base_right->translate(glm::vec3(1, 0, 0));
 	robot_arm_upper->translate(glm::vec3(0, 7.5f, 0));
 	robot_head->translate(glm::vec3(0, 7.5f, 0));
+	robot_claw_lower_left->translate(glm::vec3(-0.5f, 0.5f, 0));
+	robot_claw_lower_right->translate(glm::vec3(0.5f, 0.5f, 0));
+	robot_claw_upper_left->translate(glm::vec3(0, 0.5f, 0));
+	robot_claw_upper_right->translate(glm::vec3(0, 0.5f, 0));
 
 	return robot;
 }
@@ -156,29 +237,43 @@ bool mainTest() {
 		camera_component->set_projection(app->get_resolution());
 	}
 
-	currently_selected_part = 0;
+	// This define how fast the robot can move over the surface
+	float robot_speed = 10.0f;
+
+	// We start selecting the robot root
+	currently_selected_part = RobotParts::ROBOT;
+
+	// Create the robot and find all the parts we want to interact with.
 	auto robot = create_robot(entity_manager);
-	auto robot_arm_lower = find_part(robot.get(), RobotParts::ARM_LOWER);
-	auto robot_arm_upper = find_part(robot.get(), RobotParts::ARM_UPPER);
-	auto robot_head = find_part(robot.get(), RobotParts::HEAD);
+	auto robot_arm_lower = find_part(robot.get(),			RobotParts::ARM_LOWER);
+	auto robot_arm_upper = find_part(robot.get(),			RobotParts::ARM_UPPER);
+	auto robot_head = find_part(robot.get(),				RobotParts::HEAD);
+	auto robot_claw_lower_left = find_part(robot.get(),		RobotParts::CLAW_LOWER_LEFT);
+	auto robot_claw_lower_right = find_part(robot.get(),	RobotParts::CLAW_LOWER_RIGHT);
+	auto robot_claw_upper_left = find_part(robot.get(),		RobotParts::CLAW_UPPER_LEFT);
+	auto robot_claw_upper_right = find_part(robot.get(),	RobotParts::CLAW_UPPER_RIGHT);
 	
+	// Hide the cursor since we have an FPS Camera
 	app->hide_cursor();
+
+	// We listen to keyboard input events when we want single-key-press handling, like we want
+	// for changing the currently selected robot part.
 	auto keyboard_slot = app->sign_keyboard().connect(
 		[&](int key, int scancode, int action, int mods) {
 			bool new_robot_part_selected = false;
 
 			// Select a robot part
 			if (key == GLFW_KEY_KP_ADD && action == GLFW_PRESS) {
-				currently_selected_part++;
+				currently_selected_part += selection_jump_up((RobotParts)currently_selected_part);
 				new_robot_part_selected = true;
 			}
 			else if (key == GLFW_KEY_KP_SUBTRACT && action == GLFW_PRESS) {
-				currently_selected_part--;
+				currently_selected_part -= selection_jump_down((RobotParts)currently_selected_part);
 				new_robot_part_selected = true;
 			}
 
-			if (new_robot_part_selected) {
-				
+			if (new_robot_part_selected) 
+			{	
 				// Cycle the selected parts within bounds
 				if (currently_selected_part >= RobotParts::ROBOT_PARTS_COUNT) {
 					currently_selected_part = 0;
@@ -202,24 +297,29 @@ bool mainTest() {
 			return;
 		}
 
-		// If the robot's root is selected, we move the robot about
+		// If the robot's root is selected, we move the robot about the surface
 		if (currently_selected_part == RobotParts::ROBOT) {
 			if (app->is_key_down(GLFW_KEY_UP)) {
-				robot->translate(glm::vec3(0, 0, -1) * dt);
+				robot->translate(glm::vec3(0, 0, -1) * dt * robot_speed);
 			}
 			else if (app->is_key_down(GLFW_KEY_DOWN)) {
-				robot->translate(glm::vec3(0, 0, 1) * dt);
+				robot->translate(glm::vec3(0, 0, 1) * dt * robot_speed);
 			}
 			if (app->is_key_down(GLFW_KEY_LEFT)) {
-				robot->translate(glm::vec3(-1, 0, 0) * dt);
+				robot->translate(glm::vec3(-1, 0, 0) * dt * robot_speed);
 			}
 			else if (app->is_key_down(GLFW_KEY_RIGHT)) {
-				robot->translate(glm::vec3(1, 0, 0) * dt);
+				robot->translate(glm::vec3(1, 0, 0) * dt * robot_speed);
 			}
 		}
 
-		// If the base of the robot is selected, we rotate the robot left or right
-		else if (currently_selected_part == RobotParts::BASE_LEFT || currently_selected_part == RobotParts::BASE_RIGHT) {
+		// If the base or arms of the robot is selected, we rotate the robot left or right
+		if (robot && 
+			(currently_selected_part == RobotParts::BASE_LEFT || 
+			currently_selected_part == RobotParts::BASE_RIGHT ||
+			currently_selected_part == RobotParts::ARM_LOWER ||
+			currently_selected_part == RobotParts::ARM_UPPER))
+		{
 			if (app->is_key_down(GLFW_KEY_LEFT)) {
 				robot->rotate(glm::angleAxis(dt, glm::vec3(0, 1, 0)));
 			}
@@ -228,8 +328,11 @@ bool mainTest() {
 			}
 		}
 
-		// If the lower arm of the robot is selected, we rotate the lower arm up or down
-		else if (robot_arm_lower && currently_selected_part == RobotParts::ARM_LOWER)
+		// If the lower arm or base of the robot is selected, we rotate the lower arm up or down
+		if (robot_arm_lower && 
+			(currently_selected_part == RobotParts::ARM_LOWER ||
+			currently_selected_part == RobotParts::BASE_LEFT || 
+			currently_selected_part == RobotParts::BASE_RIGHT))
 		{
 			if (app->is_key_down(GLFW_KEY_UP)) {
 				robot_arm_lower->rotate(glm::angleAxis(dt, glm::vec3(1, 0, 0)));
@@ -240,7 +343,8 @@ bool mainTest() {
 		}
 
 		// If the upper arm of the robot is selected, we rotate the lower arm up or down
-		else if (robot_arm_upper && currently_selected_part == RobotParts::ARM_UPPER)
+		if (robot_arm_upper && 
+			(currently_selected_part == RobotParts::ARM_UPPER))
 		{
 			if (app->is_key_down(GLFW_KEY_UP)) {
 				robot_arm_upper->rotate(glm::angleAxis(dt, glm::vec3(1, 0, 0)));
@@ -250,13 +354,51 @@ bool mainTest() {
 			}
 		}
 
-		// If the head of the robot is selected, we rotate the robot head left or right
-		else if (robot_head && currently_selected_part == RobotParts::HEAD) {
+		// If the head or the claws of the robot is selected, we rotate the robot head left or right
+		if (robot_head && 
+			(currently_selected_part == RobotParts::HEAD || 
+			currently_selected_part == RobotParts::CLAW_UPPER_LEFT ||
+			currently_selected_part == RobotParts::CLAW_UPPER_RIGHT ||
+			currently_selected_part == RobotParts::CLAW_LOWER_LEFT ||
+			currently_selected_part == RobotParts::CLAW_LOWER_RIGHT)) 
+		{
 			if (app->is_key_down(GLFW_KEY_LEFT)) {
 				robot_head->rotate(glm::angleAxis(dt, glm::vec3(0, 1, 0)));
 			}
 			else if (app->is_key_down(GLFW_KEY_RIGHT)) {
 				robot_head->rotate(glm::angleAxis(dt, glm::vec3(0, -1, 0)));
+			}
+
+			// We also rotate the claws in or out, in a gripping motion.
+			if (robot_claw_upper_left &&
+				robot_claw_upper_right &&
+				robot_claw_lower_left &&
+				robot_claw_lower_right) 
+			{
+				if (currently_selected_part == RobotParts::HEAD ||
+					currently_selected_part == RobotParts::CLAW_LOWER_LEFT ||
+					currently_selected_part == RobotParts::CLAW_LOWER_RIGHT) 
+				{
+					if (app->is_key_down(GLFW_KEY_UP)) {
+						robot_claw_lower_left->rotate(glm::angleAxis(dt, glm::vec3(0, 0, -1)));
+						robot_claw_lower_right->rotate(glm::angleAxis(dt, glm::vec3(0, 0, 1)));
+					}
+					else if (app->is_key_down(GLFW_KEY_DOWN)) {
+						robot_claw_lower_left->rotate(glm::angleAxis(dt, glm::vec3(0, 0, 1)));
+						robot_claw_lower_right->rotate(glm::angleAxis(dt, glm::vec3(0, 0, -1)));
+					}
+				}
+				else
+				{
+					if (app->is_key_down(GLFW_KEY_UP)) {
+						robot_claw_upper_left->rotate(glm::angleAxis(dt, glm::vec3(0, 0, -1)));
+						robot_claw_upper_right->rotate(glm::angleAxis(dt, glm::vec3(0, 0, 1)));
+					}
+					else if (app->is_key_down(GLFW_KEY_DOWN)) {
+						robot_claw_upper_left->rotate(glm::angleAxis(dt, glm::vec3(0, 0, 1)));
+						robot_claw_upper_right->rotate(glm::angleAxis(dt, glm::vec3(0, 0, -1)));
+					}
+				}
 			}
 		}
 	});
