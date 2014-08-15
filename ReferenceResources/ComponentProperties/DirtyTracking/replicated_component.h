9097ef599feaf437a325cdd53424e282b7c9bda4
Pål Trefall <admin@ptrefall.com>
2014-08-15 07:04:35 +0200

    We don't need this any longer I think :)

--- ReferenceResources/ComponentProperties/DirtyTracking/replicated_component.h
+++
@@ -1,7 +0,0 @@
-
-#pragma once
-
-// This is just a marker interface to tell if a component is replicated to clients
-class ReplicatedComponent
-{
-};
--- ReferenceResources/Entity/GameObjectManagerInUse/definitions_template_names.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#define TEMPLATE_COMPANY		"Company"
-#define TEMPLATE_COMPANY_INFO	"CompanyInfo"
