6991ad62da5073fc181e44b88e452dbb447bcf9d
Endre Oma <endre.88.oma@gmail.com>
2014-10-06 12:29:02 +0200

    When loading PNGs via SOIL image, use LodePNG instead

--- include/GM/Framework/IO/SoilImageIO.h
+++ include/GM/Framework/IO/SoilImageIO.h
@@ -15,6 +15,10 @@ public:
 	RawImage load(const unsigned char * const undecoded_image, const unsigned int size) override;
 
 	bool save(const std::string &file_name, const RawImage& image) override;
+
+private:
+	bool is_png(const unsigned char * const undecoded_image, const unsigned int size); // if data is png we let LodePNG load it
+	RawImage load_via_LodePNG(const unsigned char * const undecoded_image, const unsigned int size);
 };
 
 } // namespace Framework
--- src/Framework/IO/SoilImageIO.cpp
+++ src/Framework/IO/SoilImageIO.cpp
@@ -5,6 +5,8 @@
 #include <cctype>
 #include <algorithm>
 
+#include "lodepng.h"
+
 namespace GM {
 namespace Framework {
 
@@ -18,6 +20,7 @@ SoilImageIO::~SoilImageIO()
 
 RawImage SoilImageIO::load(const std::string &file_name)
 {
+	//std::cout << "Will load image file " << file_name << std::endl;
 	clan::DataBuffer buffer = clan::File::read_bytes(file_name);
 	return load(buffer.get_data<unsigned char>(), buffer.get_size());
 }
@@ -29,21 +32,29 @@ RawImage SoilImageIO::load(const std::vector<unsigned char> &undecoded_image)
 
 RawImage SoilImageIO::load(const unsigned char * const undecoded_image, const unsigned int size)
 {
-	int width;
-	int height;
-	int num_channels;
-	unsigned char * decoded_data = SOIL_load_image_from_memory(undecoded_image, size, &width, &height, &num_channels, SOIL_LOAD_AUTO);
 
-	if (decoded_data == nullptr)
+	if (is_png(undecoded_image, size))
 	{
-		throw clan::Exception("Unable to decode image!");
+		return load_via_LodePNG(undecoded_image, size);
 	}
+	else
+	{
+		int width;
+		int height;
+		int num_channels;
+		unsigned char * decoded_data = SOIL_load_image_from_memory(undecoded_image, size, &width, &height, &num_channels, SOIL_LOAD_AUTO);
 
-	RawImage image(decoded_data, width, height, num_channels);
+		if (decoded_data == nullptr)
+		{
+			throw clan::Exception("Unable to decode image!");
+		}
 
-	SOIL_free_image_data(decoded_data);
+		RawImage image(decoded_data, width, height, num_channels);
 
-	return image;
+		SOIL_free_image_data(decoded_data);
+
+		return image;
+	}
 }
 
 bool SoilImageIO::save(const std::string &file_name, const RawImage &image)
@@ -77,6 +88,59 @@ bool SoilImageIO::save(const std::string &file_name, const RawImage &image)
 	successful = SOIL_save_image(file_name.c_str(), image_type, image.get_width(), image.get_height(), image.get_num_channels(), image.get_data().data());
 
 	return successful;
+}
+
+/**
+ * Inspect the assumed png's header to confirm it is a png
+ */
+bool SoilImageIO::is_png(const unsigned char *undecoded_image, const unsigned int size)
+{
+
+	LodePNGState state;
+	unsigned int w = 0, h = 0, error = 0;
+
+	lodepng_state_init(&state);
+
+	error = lodepng_inspect(&w, &h, &state, undecoded_image, size);
+
+	lodepng_state_cleanup(&state);
+
+	//if (error)
+	//{
+	//	std::cout << clan::string_format("is_png test: Unable to decode image, %1", lodepng_error_text(error)) << std::endl;
+	//}
+
+	return (error == 0); // No errors means we can decode via LodePNG
+}
+
+/**
+ * Load image data as png, as LodePNG is better at decoding png than
+ * SOIL
+ */
+RawImage SoilImageIO::load_via_LodePNG(const unsigned char *undecoded_image, const unsigned int size)
+{
+		unsigned int width = 0, height = 0, num_channels = 0;
+		unsigned int error = 0;
+
+		std::vector<unsigned char> decoded_image;
+
+		lodepng::State png_state;
+		// if you want to toy with the output bitdepth and num channels
+		// uncomment this snippet and modify values in png_state.info_raw
+		// based on data in png_state.info_png
+		//lodepng_inspect(&width, &height, &png_state, undecoded_image, size); 
+
+		error = lodepng::decode(decoded_image, width, height, png_state,
+		                        undecoded_image, size);
+
+		if (error)
+		{
+			throw clan::Exception(clan::string_format("Unable to decode image, %1", lodepng_error_text(error)));
+		}
+
+		num_channels = lodepng_get_channels(&(png_state.info_raw));
+
+		return RawImage(decoded_image, width, height, num_channels);
 }
 
 } // namespace Framework
