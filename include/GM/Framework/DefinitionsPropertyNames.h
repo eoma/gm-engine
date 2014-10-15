c00de071b548ce21977f11afce9824f32ef1c237
Endre Oma <endre.88.oma@gmail.com>
2014-10-15 21:05:51 +0200

    Fix: wrong value for constant GM_PROPERTY_OBJECT_MATRIX/-NO_SCALE

--- include/GM/Framework/DefinitionsPropertyNames.h
+++ include/GM/Framework/DefinitionsPropertyNames.h
@@ -4,9 +4,9 @@
 #define GM_PROPERTY_POSITION			"Position"			// glm::vec3
 #define GM_PROPERTY_SCALE 				"Scale"				// glm::vec3
 #define GM_PROPERTY_ORIENTATION			"Orientation"		// glm::quat
-#define GM_PROPERTY_OBJECT_MATRIX		"OjectMatrix"		// glm::mat4
+#define GM_PROPERTY_OBJECT_MATRIX		"ObjectMatrix"		// glm::mat4
 #define GM_PROPERTY_WORLD_MATRIX		"WorldMatrix"		// glm::mat4
-#define GM_PROPERTY_OBJECT_MATRIX_NO_SCALE	"OjectMatrixNoScale"	// glm::mat4
+#define GM_PROPERTY_OBJECT_MATRIX_NO_SCALE	"ObjectMatrixNoScale"	// glm::mat4
 #define GM_PROPERTY_WORLD_MATRIX_NO_SCALE	"WorldMatrixNoScale"	// glm::mat4
 
 // Culler and render system
