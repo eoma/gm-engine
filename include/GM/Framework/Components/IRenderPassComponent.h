7d8e4302fd67b92df80bca1fab20150fd371a1af
Endre Oma <endre.88.oma@gmail.com>
2014-11-26 17:43:18 +0100

    Make Framework::IRenderPassComponent declare whether a pass uses camera's render texture

--- include/GM/Framework/Components/IRenderPassComponent.h
+++ include/GM/Framework/Components/IRenderPassComponent.h
@@ -10,6 +10,11 @@
 #include <memory>
 
 namespace GM {
+
+	namespace Core {
+		class Texture; typedef std::shared_ptr<Texture> TexturePtr;
+	}
+
 	namespace Framework {
 
 		// Forward declarations
@@ -25,6 +30,11 @@ namespace GM {
 			// Render pass component may do whatever they want, please be kind.
 			// Render pass components will _always_ be used on a camera.
 			virtual void pass(RenderSystem * const render_system) = 0;
+
+			// Post process passes should accept an input source and an output source
+			virtual bool uses_render_texture_from_camera() const = 0;
+			virtual void set_input_texture(const Core::TexturePtr &input_texture) = 0;
+			virtual void set_output_texture(const Core::TexturePtr &output_texture) = 0;
 		};
 
 	} // namespace Framework
