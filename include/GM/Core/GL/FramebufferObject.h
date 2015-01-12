d04f190f581f1627ce718958f5d126fd5be90a7f
Endre Oma <endre.88.oma@gmail.com>
2015-01-12 09:12:51 +0100

    Updated Core::FramebufferObject

--- include/GM/Core/GL/FramebufferObject.h
+++ include/GM/Core/GL/FramebufferObject.h
@@ -26,27 +26,47 @@ public:
 	void bind();
 	void unbind();
 
+	// Will fetch texture handles from render textures and buffers and recheck.
+	// Useful if texture handles has changed in the shared pointers
+	void rebuild();
+
+	/**
+	 * Get and set draw buffer assignment.
+	 * The key in the map refers to draw buffer index (unsigned int, the value refers to the actual buffer or texture attachment.
+	 * May be easier to manage than standard arrays/vectors
+	 *
+	 * Draw buffer index is specified in the fragment shader.
+	 * Example: layout(location = some_index) out vec4 colour;
+	 */
+	void set_draw_buffer_as_map(const std::map<unsigned int, GLenum> &draw_buffer_assignment);
+	const std::map<unsigned int, GLenum> &get_draw_buffer_as_map() const;
+
 	// Checks if this is a valid framebuffer
 	void check();
 
-	void add(unsigned int attachment, const RenderbufferObjectPtr &render_buffer);
-	void add(unsigned int attachment, const TexturePtr &render_texture);
+	void add(GLenum attachment, const RenderbufferObjectPtr &render_buffer);
+	void add(GLenum attachment, const TexturePtr &render_texture);
 
-	bool has_attachment(unsigned int attachment) const;
+	bool has_attachment(GLenum attachment) const;
 
-	bool has_render_texture(unsigned int attachment) const;
-	bool has_render_buffer(unsigned int attachment) const;
+	bool has_render_texture(GLenum attachment) const;
+	bool has_render_buffer(GLenum attachment) const;
 
-	TexturePtr get_render_texture(unsigned int attachment) const { return render_textures.at(attachment); }
-	RenderbufferObjectPtr get_render_buffer(unsigned int attachment) const { return render_buffers.at(attachment); }
+	TexturePtr get_render_texture(GLenum attachment) const { return render_textures.at(attachment); }
+	RenderbufferObjectPtr get_render_buffer(GLenum attachment) const { return render_buffers.at(attachment); }
 
-	glm::vec4 pick(int x, int y, unsigned int attachment);
+	glm::vec4 pick(int x, int y, GLenum attachment);
+
+private:
+	void set_render_texture(GLenum attachment, const TexturePtr &render_texture);
+	void set_render_buffer(GLenum attachment, const RenderbufferObjectPtr &render_buffer);
 
 private:
 	unsigned int handle;
 
-	std::map<unsigned int, TexturePtr> render_textures;
-	std::map<unsigned int, RenderbufferObjectPtr> render_buffers;
+	std::map<GLenum, TexturePtr> render_textures;
+	std::map<GLenum, RenderbufferObjectPtr> render_buffers;
+	std::map<unsigned int, GLenum> draw_buffer_assignment;
 };
 
 } // namespace Core
--- src/Core/GL/FramebufferObject.cpp
+++ src/Core/GL/FramebufferObject.cpp
@@ -30,26 +30,39 @@ void FramebufferObject::unbind()
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
 
-void FramebufferObject::add(unsigned int attachment, const RenderbufferObjectPtr &render_buffer)
+void FramebufferObject::rebuild()
+{
+	// Update render buffers
+	for (auto iter : render_buffers)
+	{
+		set_render_buffer(iter.first, iter.second);
+	}
+
+	// Update render textures
+	for (auto iter : render_textures)
+	{
+		set_render_texture(iter.first, iter.second);
+	}
+
+	check();
+}
+
+void FramebufferObject::add(GLenum attachment, const RenderbufferObjectPtr &render_buffer)
 {
 	if (has_render_texture(attachment)) {
 		render_textures.erase(attachment);
 	}
 	render_buffers[attachment] = render_buffer;
-
-	render_buffer->bind();
-	glFramebufferRenderbuffer(GL_FRAMEBUFFER, attachment, GL_RENDERBUFFER, render_buffer->get_handle());
+	set_render_buffer(attachment, render_buffer);
 }
 
-void FramebufferObject::add(unsigned int attachment, const TexturePtr &render_texture)
+void FramebufferObject::add(GLenum attachment, const TexturePtr &render_texture)
 {
 	if (has_render_buffer(attachment)) {
 		render_buffers.erase(attachment);
 	}
 	render_textures[attachment] = render_texture;
-
-	render_texture->bind();
-	glFramebufferTexture2D(GL_FRAMEBUFFER, attachment, render_texture->get_type(), render_texture->get_handle(), 0);
+	set_render_texture(attachment, render_texture);
 }
 
 void FramebufferObject::check()
@@ -59,22 +72,22 @@ void FramebufferObject::check()
 		throw std::runtime_error("Can't initialize an FramebufferObject render texture. FramebufferObject initialization failed.");
 }
 
-bool FramebufferObject::has_attachment(unsigned int attachment) const
+bool FramebufferObject::has_attachment(GLenum attachment) const
 {
 	return has_render_buffer(attachment) || has_render_texture(attachment);
 }
 
-bool FramebufferObject::has_render_buffer(unsigned int attachment) const
+bool FramebufferObject::has_render_buffer(GLenum attachment) const
 {
 	return render_buffers.find(attachment) != render_buffers.end();
 }
 
-bool FramebufferObject::has_render_texture(unsigned int attachment) const
+bool FramebufferObject::has_render_texture(GLenum attachment) const
 {
 	return render_textures.find(attachment) != render_textures.end();
 }
 
-glm::vec4 FramebufferObject::pick(int x, int y, unsigned int attachment)
+glm::vec4 FramebufferObject::pick(int x, int y, GLenum attachment)
 {
 	//TODO: Should rather get the value from GL_MAX_COLOR_ATTACHMENTS for max attachment id
 	if(attachment < GL_COLOR_ATTACHMENT0 || attachment > GL_COLOR_ATTACHMENT15)
@@ -100,6 +113,37 @@ glm::vec4 FramebufferObject::pick(int x, int y, unsigned int attachment)
 
 	//Return color data
 	return glm::vec4(data[0], data[1], data[2], data[3]);
+}
+
+void FramebufferObject::set_draw_buffer_as_map(const std::map<unsigned int, GLenum> &new_draw_buffer_assignment)
+{
+	draw_buffer_assignment = new_draw_buffer_assignment;
+	unsigned int biggest = draw_buffer_assignment.rbegin()->first;
+	std::vector<GLenum> draw_buffers(biggest + 1, GL_NONE);
+
+	for (auto iter : draw_buffer_assignment)
+	{
+		draw_buffers[iter.first] = iter.second;
+	}
+
+	glDrawBuffers(draw_buffers.size(), draw_buffers.data());
+}
+
+const std::map<unsigned int, GLenum> &FramebufferObject::get_draw_buffer_as_map() const
+{
+	return draw_buffer_assignment;
+}
+
+void FramebufferObject::set_render_texture(GLenum attachment, const TexturePtr &render_texture)
+{
+	render_texture->bind();
+	glFramebufferTexture2D(GL_FRAMEBUFFER, attachment, render_texture->get_type(), render_texture->get_handle(), 0);
+}
+
+void FramebufferObject::set_render_buffer(GLenum attachment, const RenderbufferObjectPtr &render_buffer)
+{
+	render_buffer->bind();
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, attachment, GL_RENDERBUFFER, render_buffer->get_handle());
 }
 
 } // namespace Core
