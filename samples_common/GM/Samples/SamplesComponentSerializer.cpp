1d0d1340de679b13fadecbdd81b8cc505aaa13a9
Endre Oma <endre.88.oma@gmail.com>
2015-01-27 13:22:17 +0100

    Make SamplesComponentSerializer register component creator functions in Main's MainComponentSerializer

--- samples_common/GM/Samples/SamplesComponentSerializer.cpp
+++ samples_common/GM/Samples/SamplesComponentSerializer.cpp
@@ -8,20 +8,22 @@
 using namespace GM;
 using namespace Samples;
 
-SamplesComponentSerializer::SamplesComponentSerializer(const Application::MainPtr &app) : app(app.get()) {
-	slots.connect(
-		app->get_entity_manager()->register_component_serializer_signal(),
-		this, &SamplesComponentSerializer::create_and_add_component);
-}
+SamplesComponentSerializer::SamplesComponentSerializer(const Application::MainPtr &app) {
+	app->get_component_serializer()->add_component_creator(Samples::FPSControllerComponent::get_static_type(),
+		[app](const Framework::EntityPtr &owner) {
+			owner->create_component<Samples::FPSControllerComponent>(app.get());
+		}
+	);
 
-void SamplesComponentSerializer::create_and_add_component(const Framework::EntityPtr &owner, const std::string &type, const std::string &/*name*/) {
-	if (type == Samples::FPSControllerComponent::get_static_type()) {
-		owner->create_component<Samples::FPSControllerComponent>(app);
-	}
-	else if (type == Samples::IdleRotationComponent::get_static_type()) {
-		owner->create_component<Samples::IdleRotationComponent>();
-	}
-	else if (type == Samples::StarfieldComponent::get_static_type()) {
-		owner->create_component<Samples::StarfieldComponent>(app);
-	}
+	app->get_component_serializer()->add_component_creator(Samples::IdleRotationComponent::get_static_type(),
+		[app](const Framework::EntityPtr &owner) {
+			owner->create_component<Samples::IdleRotationComponent>();
+		}
+	);
+	
+	app->get_component_serializer()->add_component_creator(Samples::StarfieldComponent::get_static_type(),
+		[app](const Framework::EntityPtr &owner) {
+			owner->create_component<Samples::StarfieldComponent>(app.get());
+		}
+	);
 }
--- samples_common/GM/Samples/SamplesComponentSerializer.h
+++ samples_common/GM/Samples/SamplesComponentSerializer.h
@@ -8,11 +8,6 @@ namespace GM {
 		class SamplesComponentSerializer {
 		public:
 			SamplesComponentSerializer(const Application::MainPtr &app);
-			void create_and_add_component(const Framework::EntityPtr &owner, const std::string &type, const std::string &/*name*/);
-
-		private:
-			Application::Main *app;
-			clan::SlotContainer slots;
 		};
 		typedef std::shared_ptr<SamplesComponentSerializer> SamplesComponentSerializerPtr;
 	}
