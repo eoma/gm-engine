1d6b8e3fe251a53557fcccd6865acc846535f30f
Endre Oma <endre.88.oma@gmail.com>
2015-02-09 22:12:04 +0100

    Document the systems some more

--- include/GM/Framework/Systems/RenderSystem.h
+++ include/GM/Framework/Systems/RenderSystem.h
@@ -26,46 +26,78 @@ public:
 	void add_light(Light *light);
 	void remove_light(Light *light);
 
-	// Perform render
+	/**
+	 * Perform a render
+	 */
 	void render();
 
-	// render elements with shaders for a specific pass, elements from certain layers.
-	// @param accepted_layers will be &ed with cameras layers.
+	/**
+	 * Render elements with shaders for a specific pass and elements from certain layers.
+	 *
+	 * @param camera the current active camera
+	 * @param render_pass_name the wanted render pass
+	 * @param accepted_layers will be &ed with cameras layers
+	 */
 	void pass(Camera * const camera, const std::string &render_pass_name, unsigned int accepted_layers);
 
+	/**
+	 * Set current render area
+	 */
 	void resize(int width, int height);
 
-	// The methods will throw an std::out_of_range exception if bucket_index > 31
+	/**
+	 * Get a list of renderables for a given layer.
+	 *
+	 * The method will throw an std::out_of_range exception if bucket_index > 31
+	 */
 	const std::vector<IRenderable*> &get_bucket(const unsigned int bucket_index);
 
-	// Get all registered cameras
+	/**
+	 * Get all registered cameras
+	 */
 	const std::vector<Camera*> &get_cameras() { return cameras; }
 
-	// Get all registered cameras in a render layer
+	/**
+	 * Get all registered cameras in a render layer
+	 */
 	const std::vector<Camera*> &get_cameras(const unsigned int layer_index);
 
+	/**
+	 * Return a list of all registered lights
+	 */
 	const std::vector<Light *> &get_lights() const { return lights; }
 
 public:
 
-	// Should this be moved to a utility class?
+	/**
+	 * Convert a single unsigned 32-bit integer (n = 2^a + 2^b + ..., a < b < ...)
+	 * in to a list of exponents of base 2 (n is converted to [a, b, ...])
+	 */
 	static std::vector<unsigned int> bit_index_maker(const unsigned int bits);
 
 private:
-	// Position is interpreted as layer. Position 0 is interpreted as layer 1,
-	// position 1 is layer 2, and so forth.
+	/**
+	 * Position is interpreted as layer. Position 0 is interpreted as layer 1,
+	 * position 1 is layer 2, and so forth.
+	 */
 	std::array<std::vector<IRenderable*>, std::numeric_limits<unsigned int>::digits> buckets;
 
-	// Follows almost same structure as the buckets structure.
-	// Position is determined by layer. The order in the layer's
-	//  vector is depth sorted.
+	/**
+	 * Follows almost same structure as the buckets structure.
+	 * Position is determined by layer. The order in the layer's
+	 * vector is depth sorted.
+	 */
 	std::array<std::vector<Camera*>, std::numeric_limits<unsigned int>::digits> cameras_in_layers;
 
-	// Depth sorted cameras
+	/**
+	 * Depth sorted cameras. Determines the render order.
+	 */
 	std::vector<Camera*> cameras;
 
-	// We track lights in the scene in a special list, since
-	// lights affect all geometry in scene.
+	/**
+	 * We track lights in the scene in a special list, since
+	 * lights affect all geometry in scene.
+	 */
 	std::vector<Light*> lights;
 };
 
--- include/GM/Framework/Systems/SceneSystem.h
+++ include/GM/Framework/Systems/SceneSystem.h
@@ -14,7 +14,11 @@ public:
 	SceneSystem();
 	~SceneSystem();
 
-	// Prepare the transforms
+	/**
+	 * Traverse the scene tree and
+	 *
+	 * @param force_update force all transforms to update it's values whether dirty or not
+	 */
 	void prepare(bool force_update = false);
 
 	void add(Transform * const transform);
@@ -28,10 +32,17 @@ public:
 	void remove(Transform * const transform, Transform * const parent,
 		std::function<void(Transform * const child, Transform * const parent)> remove_callback);
 
+	/**
+	 * Get a list of all parentless transforms. These forms the root of the tree.
+	 *
+	 * @return list of parentless transforms.
+	 */
 	const std::vector<Transform*> &get_transforms() const;
 
 private:
-	// Walk through the tree and update object and world matrices, if necessary
+	/**
+	 * Walk through the tree and update object and world matrices, if necessary.
+	 */
 	void prepare(Transform *transform, bool must_update_world = false, bool force_update = false);
 
 private:
